0e51b5957c5ddd968462e1f58058dc6a9a4f5a68
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2023-10-12 05:07:52 +0000

    [LeetCode Sync] Runtime - 11 ms (0.25%), Memory - 13.9 MB (100.00%)

--- solutions/0034-find-first-and-last-position-of-element-in-sorted-array/solution.cpp
+++ solutions/0034-find-first-and-last-position-of-element-in-sorted-array/solution.cpp
@@ -1,72 +1,20 @@
 class Solution {
 public:
-
-    int binarySearchStart(vector<int>& nums,int left,int right,int target){
-      if(left>right){
-        return -1;
-      }
-      int mid = left + (right-left)/2;
-      
-      if(mid==0 and nums[mid]==target){
-        return mid;
-      }
-      else if(mid==0 and nums[mid]>target){
-        return -1;
-      }
-      else if(mid==0 and nums[mid]<target){
-        return binarySearchStart(nums,mid+1,right,target);
-      }
-
-      if(nums[mid]==target and nums[mid-1]!=target){
-        return mid;
-      }
-      else if(nums[mid]==target and nums[mid-1]==target){
-        return binarySearchStart(nums,left,mid-1,target);
-      }
-      else if(nums[mid]>target){
-        return binarySearchStart(nums,left,mid-1,target);
-      }
-      else{
-       return binarySearchStart(nums,mid+1,right,target);
-      }
-      return -1;
-    }
-
-
-        int binarySearchEnd(vector<int>& nums,int left,int right,int target){
-      if(left>right){
-        return -1;
-      }
-      int mid = left + (right-left)/2;
-      if(mid==nums.size()-1 and nums[mid]==target){
-        return mid;
-      }
-      else if(mid==nums.size()-1 and nums[mid]!=target){
-        return -1;
-      }
-      if(nums[mid]==target and nums[mid+1]!=target){
-        return mid;
-      }
-      else if(nums[mid]==target and nums[mid+1]==target){
-        return binarySearchEnd(nums,mid+1,right,target);
-      }
-      else if(nums[mid]>target){
-        return binarySearchEnd(nums,left,mid-1,target);
-      }
-      else{
-       return binarySearchEnd(nums,mid+1,right,target);
-      }
-      return -1;
-    }
-
     vector<int> searchRange(vector<int>& nums, int target) {
-       if(nums.size()==0){
-         return vector<int>({-1,-1});
-       }
-        int start = binarySearchStart(nums,0,nums.size()-1,target);
-        int end = binarySearchEnd(nums,0,nums.size()-1,target);
-        vector<int> m ({start,end});
+         vector<int>ans;
+        bool is_there = binary_search(nums.begin(),nums.end(),target);
+        if(is_there){
+        int start = lower_bound(nums.begin(),nums.end(),target) - nums.begin();
+        int end = upper_bound(nums.begin(),nums.end(),target)- nums.begin() - 1;
+       
+        ans.push_back(start);
+        ans.push_back(end);
+        return ans;
+        }
+        ans.push_back(-1);
+                ans.push_back(-1);
 
-        return m;
+        return ans;
+        
     }
 };
