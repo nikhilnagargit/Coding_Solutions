373d971c35e2d7e5e05eebc85090ecaa3c6fa2d8
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2023-10-25 10:55:57 +0000

    [LeetCode Sync] Runtime - 23 ms (1.46%), Memory - 25.2 MB (100.00%)

---
+++ solutions/0189-rotate-array/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void reverse(vector<int>&nums,int left,int right){
+        int i=left;
+        int j=right;
+        while(i<j){
+            swap(nums[i],nums[j]);
+            i++;
+            j--;
+        }
+    }
+    void rotate(vector<int>& nums, int k) {
+        k = k%nums.size();
+        reverse(nums,0,nums.size()-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,nums.size()-1);
+    }
+};
