f26e7700ea53c996cdd6ed6998be55acd9496608
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2023-11-01 05:16:20 +0000

    [LeetCode Sync] Runtime - 52 ms (14.82%), Memory - 48.7 MB (99.99%)

--- solutions/0055-jump-game/solution.cpp
+++ solutions/0055-jump-game/solution.cpp
@@ -1,31 +1,23 @@
 class Solution {
 public:
-    
-    bool solve(vector<int>& nums, int i, vector<int>& dp){
-        if(i==nums.size()-1){
-            return 1;
+
+    bool canJump(vector<int>& nums) {
+        int max_index_reachable = 0;
+        int target = nums.size()-1;
+        if(nums.size()==1){
+            return true;
         }
-        
-        if(nums[i]==0){
-            return false;
-        }
-         
-        for(int k=1;k<=nums[i];k++){
-            if(dp[i+k]==-1){
-                dp[i+k]= solve(nums,i+k,dp);
+        for(int i=0;i<target;i++){
+            if(max_index_reachable<i){
+                return false;
             }
-            if(dp[i+k]==1){
-                return 1;
+            if(max_index_reachable<i+nums[i]){
+                max_index_reachable= i+nums[i];
+            }
+            if(max_index_reachable>=target){
+                return true;
             }
         }
-        
-        return 0;
-    }
-    
-    
-    bool canJump(vector<int>& nums) {        
-        vector<int> dp(nums.size(),-1);
-        dp[0] = 1;
-        return solve(nums,0,dp);
+        return false;
     }
 };
