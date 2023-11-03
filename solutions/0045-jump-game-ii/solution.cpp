6c064a8d9504cceb1f49439c0ad601ee7a04c44f
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2023-11-03 10:57:38 +0000

    [LeetCode Sync] Runtime - 8 ms (25.78%), Memory - 16.9 MB (100.00%)

--- solutions/0045-jump-game-ii/solution.cpp
+++ solutions/0045-jump-game-ii/solution.cpp
@@ -1,30 +1,15 @@
 class Solution {
 public:
-    int jump(vector<int>& nums) {
-        int N= nums.size();
-        int dp[N];
-        for(int i=0;i<N;i++){
-            dp[i] = -1;
-        }
-        dp[0] = 0;
-        int current=0;
-        for(int i=0;i<N;i++){
-            int j=1;
-
-            while(j<=nums[i] and i+j<N){
-               
-                if(dp[i+j]==-1){
-         
-                    dp[i+j] = dp[i]+1;
-                }
-                if(i+j==N-1){
-                    return dp[i+j];
-                }
-          
-                j++;
-            }
-        }
-        return dp[N-1];
-        
-    }
+int jump(vector<int>& nums) {
+	int n = size(nums), i = 0, maxReachable = 0, lastJumpedPos = 0, jumps = 0;
+	while(lastJumpedPos < n - 1) { 
+		maxReachable = max(maxReachable, i + nums[i]);  
+		if(i == lastJumpedPos) {			 
+			lastJumpedPos = maxReachable;    
+			jumps++;                      
+		}            
+		i++;
+	}
+	return jumps;
+}
 };
