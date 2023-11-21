8cb8e4d7a55d25be9289829b574040f8d9387eb0
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2023-11-21 05:14:00 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 6.4 MB (100.00%)

--- solutions/0050-powx-n/solution.cpp
+++ solutions/0050-powx-n/solution.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
-    double myPow(double x, long long n) {
-        if(n==0){
-            return 1;
+    double myPow(double x, int n) {
+        if (n == 0)return 1;
+        long long N = n;
+        if (n < 0) {
+            x = 1 / x;
+            N = -N;
         }
-        
-        if(n<0){
-            return 1/myPow(x,-n);
-        }
-        
-       if(n%2==0){
-           return myPow(x*x,n/2);
-       }
-            return x*myPow(x,n-1);
 
+    if (N & 1) {
+        return x * myPow(x * x, N / 2);
+    } else {
+        return myPow(x * x, N / 2);
     }
+        }
     
 };
