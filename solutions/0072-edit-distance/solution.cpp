2129a194e7ba7e3ed68f4f5ad42a3e847ba442eb
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2023-11-23 04:15:41 +0000

    [LeetCode Sync] Runtime - 7 ms (63.70%), Memory - 9.4 MB (100.00%)

--- solutions/0072-edit-distance/solution.cpp
+++ solutions/0072-edit-distance/solution.cpp
@@ -1,31 +1,19 @@
 class Solution {
 public:
-    int minDistance(string word1, string word2) {
-        int m = word1.size();
-        int n = word2.size();
-        
-        int dp[m+1][n+1];
-        
-        dp[0][0] =0;
-        for(int i=1;i<=m;i++){
-            dp[i][0] = i;
+    int solve(int i,int j, string& w1,string& w2,vector<vector<int>>&m){
+        if(j==w2.size())return (w1.size()-i);
+        if(i==w1.size())return (w2.size()-j);
+        if(m[i][j]!=-1){
+            return m[i][j];
         }
+        if(w1[i]==w2[j]){
+            return m[i][j]=solve(i+1,j+1,w1,w2,m);
+        }
+        return m[i][j]=1+min(solve(i,j+1,w1,w2,m),min(solve(i+1,j+1,w1,w2,m),solve(i+1,j,w1,w2,m)));
+    }
 
-        for(int i=1;i<=n;i++){
-            dp[0][i] = i;
-        }
-        
-        for(int i=1;i<=m;i++){
-            for(int j=1;j<=n;j++){
-                if(word1[i-1]==word2[j-1]){
-                    dp[i][j] = dp[i-1][j-1];
-                }
-                else{
-                    dp[i][j] = 1 + min(min(dp[i-1][j],dp[i][j-1]),dp[i-1][j-1]);
-                }
-            }
-        }
-        return dp[m][n];
-        
+    int minDistance(string word1, string word2) {
+        vector<vector<int>> m (word1.size()+1,vector<int>(word2.size()+1,-1));
+        return solve(0,0,word1,word2,m);
     }
 };
