0d9d68f9b41ea035fcdc07900a731975f4bcc188
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2023-11-29 03:14:49 +0000

    [LeetCode Sync] Runtime - 4 ms (0.49%), Memory - 6.6 MB (100.00%)

--- solutions/0191-number-of-1-bits/solution.cpp
+++ solutions/0191-number-of-1-bits/solution.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        int count =0;
+        int count=0;
         for(int i=0;i<32;i++){
-            if((n&(1<<i))!=0){
+            if((1<<i)&n){
                 count++;
             }
         }
