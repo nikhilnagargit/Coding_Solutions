3d7c7f51948aff06a3e2c0db10d371f56da57fbc
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2023-11-30 18:57:59 +0000

    [LeetCode Sync] Runtime - 3 ms (1.08%), Memory - 6.4 MB (100.00%)

---
+++ solutions/1732-minimum-one-bit-operations-to-make-integers-zero/solution.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int minimumOneBitOperations(int n) {
+        int ans=0;
+        for(int i=0;i<32;i++) if(n&(1<<i))  ans=((1<<(i+1))-1)-ans;
+        return ans;
+
+    }
+};
