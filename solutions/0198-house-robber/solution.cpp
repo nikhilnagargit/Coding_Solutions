fe70cff72fe3270644596da36b04eca020746ef8
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2023-12-15 09:07:43 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8 MB (100.00%)

--- solutions/0198-house-robber/solution.cpp
+++ solutions/0198-house-robber/solution.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
+    vector<int> dp;
+    int solve(vector<int>&nums,int i){
+        if(i>=nums.size()){
+            return 0;
+        }
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+        return dp[i]=max(nums[i]+solve(nums,i+2),solve(nums,i+1));
+    }
     int rob(vector<int>& nums) {
-        vector<int> dp(nums.size()+1,0);
-        if(nums.size()==1){
-            return nums[0];
-        }
-        if(nums.size()==2){
-            return max(nums[0],nums[1]);
-        }
-
-        dp[0] = nums[0];
-        dp[1] = max(nums[0],nums[1]);
-        for(int i=2;i<nums.size();i++)
-        {
-            dp[i] = max(dp[i-1],nums[i]+dp[i-2]);
-        }
-        return dp[nums.size()-1];
+        dp = vector<int>(nums.size(),-1);
+        return solve(nums,0);
     }
 };
