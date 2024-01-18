ccda57965ed319dcc0bf6c09443221e97a93c0e4
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2024-01-18 03:49:20 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 6.3 MB (100.00%)

--- solutions/0070-climbing-stairs/solution.cpp
+++ solutions/0070-climbing-stairs/solution.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
+  
+
     int climbStairs(int n) {
-        vector<int>dp(n+1,0);
-        dp[0] = 1;
-        dp[1] = 1;
-        for(int i=2;i<=n;i++){
-            dp[i] = dp[i-1]+dp[i-2];
+        static    vector<int> dp (46,-1);
+        if(n==0){
+            return 1;
         }
-        return dp[n];
+        if(n<0){
+            return 0;
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        return dp[n] = climbStairs(n-1) + climbStairs(n-2);
     }
 };
