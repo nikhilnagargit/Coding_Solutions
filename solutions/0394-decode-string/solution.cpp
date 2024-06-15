1c9b48d6570c577a73cd4fcc254828d3e2c4d8fa
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2024-06-15 17:35:55 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 7.4 MB (100.00%)

--- solutions/0394-decode-string/solution.cpp
+++ solutions/0394-decode-string/solution.cpp
@@ -1,57 +1,58 @@
 class Solution {
 public:
-    bool isDigit(char c){
-        if(c-'0'>=0 and c-'0'<=9){
-            return true;
-        }
-        return false;
-    }
-
     string decodeString(string s) {
-        stack<string> stk;
-        int i=0;
+        stack<char> char_stack;
+        stack<int> num_stack;
+        int i =0 ;
         while(i<s.size()){
-            if(isDigit(s[i])){
-                string count = "";
-                while(isDigit(s[i])){
-                    count += s[i];
-                    i++;
-                }
-                i--;
-                stk.push(count);
+            // cout<<i<<" "
+            int num = 0;
+            while(i<s.size() and s[i]-'0'>=0 and s[i]-'0'<=9){
+                num = num*10 + s[i]-'0';
+                i++;
             }
+            if(num>0)
+            num_stack.push(num);
+            // cout<<num<<',';
+            if(i<s.size() and s[i]=='['){
+                char_stack.push('[');
+                i++;
+            }
+            while((i<s.size()) and (s[i]-'a'>=0) and (s[i]-'a'<=26)){
+                char_stack.push(s[i]);
+                i++;
+            }
+            if(i<s.size() and s[i]==']'){
+                //pop the stacks
+                int num = num_stack.top();
+                num_stack.pop();
+                string tempstring = "";
+                while(char_stack.top()!='['){
 
-            // if there is a bracket
-            else if(s[i]=='['){
-                stk.push("[");
-            }
-            else if(s[i]==']'){
-                string temp = "";
-                while(!stk.empty() and stk.top()!="["){
-                    temp=stk.top()+temp;
-                    stk.pop();
+                    tempstring+= char_stack.top();char_stack.pop();
                 }
-                stk.pop();
-                int count = stoi(stk.top());
-                stk.pop();
-                string temp1 = "";
-                for(int j=0;j<count;j++){
-                    temp1+= temp;
+                char_stack.pop();
+                //reverse the tempstring
+                reverse(tempstring.begin(),tempstring.end());
+                //push the tempstring repetative times in the stack
+                for(int k=0;k<num;k++){
+                    for(char tempc:tempstring){
+                        char_stack.push(tempc);
+                    }
                 }
-                stk.push(temp1);
+                i++;
             }
-            else{
-                string t = "";
-                t+= s[i];
-                stk.push(t);
-            }
-            i++;
+        
         }
+
         string ans = "";
-        while(!stk.empty()){
-            ans = stk.top()+ans;
-            stk.pop();
+        // cout<<char_stack.top();
+        while(!char_stack.empty()){
+            ans+=char_stack.top();
+            char_stack.pop();
         }
-     return ans;
+        reverse(ans.begin(),ans.end());
+        // cout<<ans;
+        return ans;
     }
 };
