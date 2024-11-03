528d9bc6b73dd7795d918cc08ea3ac2d6650abfd
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2024-11-03 06:33:03 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.8 MB (13.79%)

---
+++ solutions/0812-rotate-string/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s==goal){
+            return true;
+        }
+        if(s.size()!=goal.size()){
+            return false;
+        }
+
+        for(int i=0;i<s.size()-1;i++){
+             s = s.substr(1) + s[0];
+             if(s==goal) return true;
+        }
+        return false;
+    }
+};
