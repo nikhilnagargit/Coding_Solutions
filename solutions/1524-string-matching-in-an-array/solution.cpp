5adbe2d8acc86a074812d28e2934c22864536d2d
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-01-07 05:56:06 +0000

    [LeetCode Sync] Runtime - 3 ms (39.55%), Memory - 11.7 MB (24.66%)

---
+++ solutions/1524-string-matching-in-an-array/solution.cpp
@@ -0,0 +1,23 @@
+
+
+class Solution {
+
+
+public:
+ static bool comp(string& a,string& b){
+    return a.size()<=b.size();
+}
+    vector<string> stringMatching(vector<string>& words) {
+        vector<string> ans;
+        sort(words.begin(),words.end(),comp);
+        for(int i=0;i<words.size();i++){
+            for(int j=i+1;j<words.size();j++){
+                if(words[j].find(words[i])!=string::npos){
+                    ans.push_back(words[i]);
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
