bc304c1188ce1e695cd8007d13494dd9c1b87ac3
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-02-20 04:12:38 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 12.4 MB (99.99%)

--- solutions/2107-find-unique-binary-string/solution.cpp
+++ solutions/2107-find-unique-binary-string/solution.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     string findDifferentBinaryString(vector<string>& nums) {
-        string result;
-
-        for (int i = 0; i < nums.size(); ++i) {
-            result += (nums[i][i] == '0' ? '1' : '0');
+        string ans = "";
+        for(int i=0;i<nums.size();i++){
+            if(nums[i][i]=='0'){
+                ans+='1';
+            }
+            else{
+                ans+='0';
+            }
         }
-
-        return result;
+        return ans;
     }
 };
