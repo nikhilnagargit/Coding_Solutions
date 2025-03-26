fa26fe9dbd8f81123051b1c022c6979df7d12548
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-03-26 16:57:46 +0000

    [LeetCode Sync] Runtime - 15 ms (84.49%), Memory - 90.2 MB (37.14%)

---
+++ solutions/2160-minimum-operations-to-make-a-uni-value-grid/solution.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int minOperations(vector<vector<int>>& grid, int x) {
+        vector<int> numsArray;
+        int result = 0;
+
+        // Flatten the grid into numsArray
+        for (int row = 0; row < grid.size(); row++) {
+            for (int col = 0; col < grid[0].size(); col++) {
+                numsArray.push_back(grid[row][col]);
+            }
+        }
+
+        int length = numsArray.size();
+
+        nth_element(numsArray.begin(), numsArray.begin() + length / 2,
+                    numsArray.end());
+
+        int finalCommonNumber = numsArray[length / 2];
+
+        for (int number : numsArray) {
+            // If the remainder when divided by x is different for any number,
+            // return -1
+            if (number % x != finalCommonNumber % x) return -1;
+            // Add the number of operations required to make the current number
+            // equal to finalCommonNumber
+            result += abs(finalCommonNumber - number) / x;
+        }
+
+        return result;
+    }
+};
