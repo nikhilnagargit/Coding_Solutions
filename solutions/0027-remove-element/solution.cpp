13cbaa63e3798ebc5a2be70ed9834c569beadad2
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-04-14 04:57:24 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 11.6 MB (99.11%)

--- solutions/0027-remove-element/solution.cpp
+++ solutions/0027-remove-element/solution.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-    int removeElement(vector<int>& arr, int val) {
+    int removeElement(vector<int>& nums, int val) {
         int i=0;
-        int j=0;
-        int k=0;
-
-        while(i<arr.size() and j<arr.size()){
-            while(j<arr.size() and arr[j]==val){
-                j++;
-                k++;
+        int curr=0;
+        int count = 0;
+        while(curr<nums.size()){
+            if(nums[curr]==val){
+                curr++;
             }
-            if(i<arr.size() and j<arr.size()){
-            arr[i]=arr[j];
-            j++;
-            i++;
+            else{
+                nums[i]=nums[curr];
+                i++;
+                curr++;
+                count++;
             }
-        
         }
-        return arr.size()-k;
+        return count;
     }
 };
