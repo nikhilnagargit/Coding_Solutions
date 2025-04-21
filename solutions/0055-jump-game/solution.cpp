34e9bbcb4916f7f98c80e6d5d3eef07f9588a455
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-04-21 03:28:39 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 52.3 MB (82.08%)

--- solutions/0055-jump-game/solution.cpp
+++ solutions/0055-jump-game/solution.cpp
@@ -2,22 +2,13 @@ class Solution {
 public:
 
     bool canJump(vector<int>& nums) {
-        int max_index_reachable = 0;
-        int target = nums.size()-1;
-        if(nums.size()==1){
-            return true;
-        }
-        for(int i=0;i<target;i++){
-            if(max_index_reachable<i){
+        int maxReachIndex = 0;
+        for(int i=0;i<nums.size();i++){
+            if(maxReachIndex<i){
                 return false;
             }
-            if(max_index_reachable<i+nums[i]){
-                max_index_reachable= i+nums[i];
-            }
-            if(max_index_reachable>=target){
-                return true;
-            }
+            maxReachIndex = max(maxReachIndex,i+nums[i]);
         }
-        return false;
+        return true;
     }
 };
