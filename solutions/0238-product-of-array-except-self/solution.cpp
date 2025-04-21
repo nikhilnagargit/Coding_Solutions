33736e7d58248849e9101351aa5e6162d9de7724
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-04-21 16:09:22 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.1 MB (31.49%)

--- solutions/0238-product-of-array-except-self/solution.cpp
+++ solutions/0238-product-of-array-except-self/solution.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int> leftprod(nums.size(),1);
-        vector<int> rightprod(nums.size(),1);
-        leftprod[0]=nums[0];
-        cout<<leftprod[0]<<",";
-        for(int i=1;i<nums.size();i++){
-            leftprod[i]=leftprod[i-1]*nums[i];
-            cout<<leftprod[i]<<",";
-        }
-        cout<<endl;
+        vector<int> PrefixV(nums.size(),1);
+        vector<int> SuffixV(nums.size(),1);
 
-        rightprod[nums.size()-1]=nums[nums.size()-1];
+        if(nums.size()<2){
+            return nums;
+        }
+        PrefixV[0] = nums[0];
+        SuffixV[nums.size()-1]= nums[nums.size()-1];
+        for(int i=1;i<nums.size();i++){
+            PrefixV[i] = PrefixV[i-1]*nums[i];
+        }
+
         for(int i=nums.size()-2;i>=0;i--){
-            rightprod[i]=rightprod[i+1]*nums[i];
+            SuffixV[i] = SuffixV[i+1]*nums[i];
         }
-        for(int i=0;i<nums.size();i++){
-            cout<<rightprod[i]<<",";
+
+        nums[0] = SuffixV[1];
+        nums[nums.size()-1] = PrefixV[nums.size()-2];
+
+        for(int i=1;i<nums.size()-1;i++){
+            nums[i] = PrefixV[i-1]*SuffixV[i+1];
         }
-        vector<int>ans;
-        for(int i=0;i<nums.size();i++){
-            int value;
-            if(i==0) value = rightprod[i+1];
-            else if(i==nums.size()-1) value = leftprod[i-1];
-            else value = leftprod[i-1]*rightprod[i+1];
-            ans.push_back(value);
-        }
-        return ans;
+        return nums;
     }
 };
