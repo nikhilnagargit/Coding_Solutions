6f9227c43dce9c84144329e1f146e047104c4e8a
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-04-22 04:16:56 +0000

    [LeetCode Sync] Runtime - 15 ms (10.06%), Memory - 13.4 MB (18.13%)

--- solutions/0013-roman-to-integer/solution.cpp
+++ solutions/0013-roman-to-integer/solution.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
     int romanToInt(string s) {
-   int ans=0;
-        unordered_map <char,int> mp{
-        {'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}};
-
-    for(int i=0;i<s.size();i++){
-        if(mp[s[i]]<mp[s[i+1]]){
-            //for cases such as IV,CM, XL, etc...
-            ans=ans-mp[s[i]];
+unordered_map<char, int> m = {
+    {'I', 1},
+    {'V', 5},
+    {'X', 10},
+    {'L', 50},
+    {'C', 100},
+    {'D', 500},
+    {'M', 1000}
+};;
+        int num = m[s[s.size()-1]];
+        
+        for(int i=s.size()-2;i>=0;i--){
+            int multiplier = 1;
+            if((s[i]=='I' and (s[i+1]=='V' or s[i+1]=='X'))
+            or (s[i]=='X' and (s[i+1]=='L' or s[i+1]=='C'))
+            or (s[i]=='C' and (s[i+1]=='D' or s[i+1]=='M'))){
+                multiplier = -1;
+            }
+            num+= m[s[i]]*multiplier;
         }
-        else{
-            ans=ans+mp[s[i]];
-        }
+        return num;
     }
-        return ans;}
 };
