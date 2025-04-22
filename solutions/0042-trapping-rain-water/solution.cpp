42ea261f5d4d9f56123130fb1ad233a044205916
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-04-22 03:31:09 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 27.3 MB (25.74%)

--- solutions/0042-trapping-rain-water/solution.cpp
+++ solutions/0042-trapping-rain-water/solution.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int n = height.size();
-        vector<int> max_in_left(n);
-        max_in_left[0]= height[0];
-        for(int i=1;i<n;i++){
-            max_in_left[i] = max(max_in_left[i-1],height[i]);
+        vector<int>lrv(height.size(),0);
+        vector<int>rlv(height.size(),0);
+        lrv[0]=height[0];
+        for(int i=1;i<height.size();i++){
+            lrv[i] = max(lrv[i-1],height[i]);
         }
-        max_in_left[n-1] = height[n-1];
-        int water=0;
-        for(int i=n-2;i>=0;i--){
-            max_in_left[i]= min(max_in_left[i],max(max_in_left[i+1],height[i]));
-            water+=max_in_left[i]-height[i];
+        rlv[height.size()-1] = height[height.size()-1];
+        for(int i=height.size()-2;i>=0;i--){
+            rlv[i] = max(rlv[i+1],height[i]);
+        }
+        int water = 0;
+        for(int i=1;i<height.size()-1;i++){
+            water += max(min(lrv[i-1],rlv[i+1])-height[i],0);
         }
         return water;
     }
