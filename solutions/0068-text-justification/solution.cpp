ab317f9f45a6ded0e11a9cf695898f8649abde30
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-04-22 19:31:32 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 11.2 MB (11.66%)

--- solutions/0068-text-justification/solution.cpp
+++ solutions/0068-text-justification/solution.cpp
@@ -1,68 +1,122 @@
 class Solution {
 public:
-    string justifylast(vector<string>& row,int rowWidth,int maxWidth){
-        string ans = row[0];
-        for(int i=1;i<row.size();i++){
-            ans+=row[i];
-        }
-        int p = ans.size();
-        while(p<maxWidth){
+    string space_multiplier(int k){
+        string ans = "";
+        while(k--){
             ans+=" ";
-            p++;
         }
         return ans;
     }
 
-    string justify(vector<string>& row,int rowWidth,int maxWidth){
-        string ans=row[0];
-        int mids = row.size()-1;
-        if(mids==0){
-         int p = ans.size();
-        while(p<maxWidth){
-            ans+=" ";
-            p++;
+    string spacify(vector<string> v,int maxWidth){
+        int curr_count = 0;
+        for(auto w:v){
+            curr_count+= w.size();
         }
-        return ans;
+        
+        string ans = "";
+        int extra_space = maxWidth-curr_count;
+        int word_count = v.size()-1;
+        if(word_count==0){
+            return v[0]+space_multiplier(extra_space);
         }
-        int rem_space = maxWidth-rowWidth;
-        int space_to_add = rem_space/mids;
-        int next_rem_space = rem_space%mids;
-        int k=0;
-        for(int i=1;i<row.size();i++){
-            string space = "";
-            if(k<next_rem_space){
-                space+=" ";
-                k++;
+        int rem = 0;
+        if(word_count!=0)
+        rem = extra_space%(word_count);
+        int append = 0;
+        if(word_count!=0)
+        append = extra_space/(word_count);
+        // cout<<append<<","<<rem<<endl;
+        for(int i=0;i<v.size();i++){
+            string w = v[i];
+            if(i!=v.size()-1){
+            if(rem>0){
+            ans+= w  + space_multiplier(append+1);
+            rem--;
             }
-            int j=space_to_add;
-            while(j--){
-                space+=" ";
+            else{
+            ans+= w  + space_multiplier(append);  
             }
-            ans+= space+row[i];
+            }
+            else{
+                ans+=w;
+            }
         }
         return ans;
     }
+
+    string leftSpacify(vector<string>v,int maxWidth){
+        int curr_count = 0;
+        for(auto w:v){
+            curr_count+= w.size();
+        }
+        
+        string ans = "";
+        int extra_space = maxWidth-curr_count;
+        int word_count = v.size()-1;
+        for(auto w:v){
+            ans+= w ;
+            if(ans.size()<maxWidth){
+                ans+=" ";
+            }
+        }
+        while(ans.size()<maxWidth){
+            ans+=" ";
+        }
+        return ans;
+    }
+
+
     vector<string> fullJustify(vector<string>& words, int maxWidth) {
-        vector<string> row;
         vector<string> ans;
-        row.push_back(words[0]);
-        int i=1;
-        int rowwidth=words[0].size();
-        while(i<words.size()){
-            string nextword  = words[i];
-            if(rowwidth+nextword.size()+1<=maxWidth){
-                rowwidth+= nextword.size()+1;
-                row.push_back(" "+nextword);
+        vector<string> v;
+
+        for(int i=0;i<words.size();i++){
+            int curr_count = 0;
+            for(auto w:v){
+                curr_count+= w.size();
+            }
+            curr_count+= v.size()-1;
+
+            if(v.size()==0){
+                if(words[i].size() == maxWidth){
+                    v.push_back(words[i]);
+                    ans.push_back(spacify(v,maxWidth));
+                    v.clear();
+                    continue;
+                }
+                else if(words[i].size() > maxWidth){
+                    ans.push_back(spacify(v,maxWidth));
+                    v.clear();
+                    v.push_back(words[i]);
+                    continue;
+                }
+                else{
+                    v.push_back(words[i]);
+                }
             }
             else{
-                ans.push_back(justify(row,rowwidth,maxWidth));
-                row.clear();
-                row.push_back(nextword);
-                rowwidth = nextword.size();
+                if((curr_count+words[i].size()+1) == maxWidth){
+                    v.push_back(words[i]);
+                    ans.push_back(spacify(v,maxWidth));
+                    v.clear();
+                    continue;
+                }
+                else if((curr_count+words[i].size()+1) > maxWidth){
+                    ans.push_back(spacify(v,maxWidth));
+                    v.clear();
+                    v.push_back(words[i]);
+                    continue;
+                }
+                else{
+                    v.push_back(words[i]);
+                }
             }
-            i++;
         }
-        ans.push_back(justifylast(row,rowwidth,maxWidth));
+        if(v.size()!=0){
+            ans.push_back(leftSpacify(v,maxWidth));
+        }
+
         return ans;
     }
 };
