de068fac4a1eba0f9eea6abc3cbef7c07b17b5f5
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-07-21 04:28:45 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 32.2 MB (5.93%)

--- solutions/0209-minimum-size-subarray-sum/solution.cpp
+++ solutions/0209-minimum-size-subarray-sum/solution.cpp
@@ -1,44 +1,21 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-
-        // first find the initial width of sliding window
-        int i=0;
-        int sum=0;
-        while(i<nums.size()){
-            sum+=nums[i];
-            if(sum>=target){
-                break;
-            }
-            i++;
-        }
-
-        if(i==nums.size()){
-            return 0;
-        }
-
         int left = 0;
-        int right = i;
-        int width = i+1;
-        int min_width = width;
+        int right=0;
+        int curr_sum = 0;
+        int min_size = INT_MAX;
 
-        while(right<nums.size() and left<=right and left<nums.size()){
-            if(sum>=target){
-                min_width = min(min_width,width);
-                width--;
+        for(int right=0;right<nums.size();right++){
+            curr_sum+= nums[right];
+            while(curr_sum>=target and left<=right){
+                min_size = min(min_size,right-left+1);
+                curr_sum-=nums[left];
                 left++;
-                if(left-1<nums.size())
-                sum = sum-nums[left-1];
             }
-            else{
-                width++;
-                right++;
-                if(right<nums.size())
-                sum = sum+nums[right];
-            }
-
         }
 
-        return min_width;
+        if(min_size==INT_MAX) return 0;
+        return min_size;
     }
 };
