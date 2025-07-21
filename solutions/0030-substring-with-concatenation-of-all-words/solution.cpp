903bda231a7f8477ed4218b177a4db9439fa70aa
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-07-21 17:40:25 +0000

    [LeetCode Sync] Runtime - 28 ms (29.51%), Memory - 24 MB (36.77%)

--- solutions/0030-substring-with-concatenation-of-all-words/solution.cpp
+++ solutions/0030-substring-with-concatenation-of-all-words/solution.cpp
@@ -1,35 +1,53 @@
 class Solution {
 public:
     vector<int> findSubstring(string s, vector<string>& words) {
-        vector<int> v;
-        unordered_map<string,int> m;
-        for(auto x:words)
-            m[x]++;
-        
-        int l=words[0].length();
-        int len=words.size()*l;
-        len=s.length()-len;
-        if(len<0)
-            return v;
-        for(int i=0;i<=len;i++)
-        {
-            unordered_map<string,int> mp=m;
-            int c=0;
-            for(int j=i;j<s.size();j+=l)
-            {
-                string x=s.substr(j,l);
-                if(mp[x]>0)
-                {
-                    c++;
-                    mp[x]--;
-                }
-                else
-                    break;
-            }
-            if(c==words.size())
-                v.push_back(i);
+        vector<int> result;
+        if(words.empty() || s.empty()) return result;
+
+        int word_len = words[0].length();
+        int num_words = words.size();
+        int concat_len = word_len * num_words;
+
+        if(s.length() < concat_len) return result;
+
+        unordered_map<string, int> word_count;
+        for(const string& word : words) {
+            word_count[word]++;
         }
-        return v;
+
+        // try every offset within a word length
+        for(int i = 0; i < word_len; ++i) {
+            int left = i, right = i;
+            unordered_map<string, int> curr_map;
+            int count = 0;
+
+            while(right + word_len <= s.length()) {
+                string word = s.substr(right, word_len);
+                right += word_len;
+
+                if(word_count.count(word)) {
+                    curr_map[word]++;
+                    count++;
+
+                    while(curr_map[word] > word_count[word]) {
+                        string left_word = s.substr(left, word_len);
+                        curr_map[left_word]--;
+                        left += word_len;
+                        count--;
+                    }
+
+                    if(count == num_words) {
+                        result.push_back(left);
+                    }
+                } else {
+                    curr_map.clear();
+                    count = 0;
+                    left = right;
+                }
+            }
+        }
+
+        return result;
     }
 };
 
