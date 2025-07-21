273fda0c935b6a616706acfa210399083d796503
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-07-21 04:01:34 +0000

    [LeetCode Sync] Runtime - 51 ms (44.57%), Memory - 29 MB (91.85%)

--- solutions/0015-3sum/solution.cpp
+++ solutions/0015-3sum/solution.cpp
@@ -1,41 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> ans;
         sort(nums.begin(),nums.end());
-
-        int i=0;
-        while(i<nums.size()-2){
-            int start = i+1;
-            int end = nums.size()-1;
-            int target = -1*nums[i];
-            while(start<end){
-                if(nums[start]+nums[end]==target){
-                    vector<int> triplet;
-                    triplet.push_back(nums[i]);
-                    triplet.push_back(nums[start]);
-                    triplet.push_back(nums[end]);
-                    ans.push_back(triplet);
-                    start++;
-                    end--;
+        vector<vector<int>>ans;
+        for(int i=0;i<nums.size()-2;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int left = i+1;
+            int right = nums.size()-1;
+            while(left<right){
+                if(nums[left]+nums[right]+nums[i]==0){
+                    ans.push_back({nums[i],nums[left],nums[right]});
+                    left++;
+                    while(nums[left]==nums[left-1] && left<right){
+                        left++;
+                    }
+                    right--;
+                    while(nums[right]==nums[right+1]&& left<right){
+                        right--;
+                    }
                 }
-                else if(nums[start]+nums[end]>target){
-                    end--;
+                else if(nums[left]+nums[right]+nums[i]>0){
+                    right--;
                 }
                 else{
-                    start++;
+                    left++;
                 }
             }
-            while(nums[i]==nums[i+1] and i<nums.size()-2){
-                i++;
-            }
-            i++;
         }
-        set<vector<int>> myset;
-        for(auto v:ans){
-            myset.insert(v);
-        }
-        vector<vector<int>> modified_ans(myset.begin(),myset.end());
-        return modified_ans;
+        return ans;
+
     }
 };
