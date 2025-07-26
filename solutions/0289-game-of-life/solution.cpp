3ebe4eee096eabe4f831c04bd2308f8039c69b60
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-07-26 08:13:18 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.8 MB (6.84%)

--- solutions/0289-game-of-life/solution.cpp
+++ solutions/0289-game-of-life/solution.cpp
@@ -1,50 +1,61 @@
 class Solution {
 public:
-    void nextstate(vector<vector<int>>& board,int i,int j){
-        vector<int>I{0,0,1,-1,1,1,-1,-1};
-        vector<int>J{1,-1,0,0,1,-1,1,-1};
-        int countof1=0;
-        for(int k=0;k<I.size();k++){
-            int first = i+I[k];
-            int second = j+J[k];
-            if(0<=first and first<board.size() and 0<=second and second<board[0].size()){
-                if(board[first][second]==1 or board[first][second]==3){
-                    countof1++;
-                }
+    void printM(vector<vector<int>>& m){
+        for(int i=0;i<m.size();i++){
+            for(int j=0;j<m[0].size();j++){
+                cout<<m[i][j]<<",";
+            }
+            cout<<endl;
+        }
+        cout<<"--------------"<<endl;
+    }
+
+    void explore(vector<vector<int>>& board,int i,int j){
+
+        vector<pair<int,int>> mp = {{0,1},{0,-1},{1,0},{-1,0},{1,1},{-1,-1},{1,-1},{-1,1}};
+        int count1s = 0;
+        for(auto& p :mp){
+            int I = i+p.first;
+            int J = j+p.second;
+            if(I>=0 and I<board.size() and J>=0 and J<board[0].size() and (board[I][J]==2 or board[I][J]==1)){
+                count1s ++;
             }
         }
-        if((board[i][j]==1 or board[i][j]==3) and countof1<2){
-            board[i][j]=3;
+
+        if(board[i][j]==1 and count1s<2){
+            board[i][j] = 2;
         }
-        else if((board[i][j]==1 or board[i][j]==3) and countof1<=3){
-            board[i][j]=1;
+        else if(board[i][j]==1 and (count1s==2 or count1s==3)){
+            board[i][j] = 1;
         }
-        else if((board[i][j]==1 or board[i][j]==3) and countof1>3){
-            board[i][j]=3;
+        else if(board[i][j]==1 and count1s>3){
+            board[i][j] = 2;
         }
-        else if((board[i][j]==0 or board[i][j]==2) and countof1==3){
-            board[i][j]=2;
-        }
-        else{
-        
+        else if(board[i][j]==0 and count1s==3){
+            board[i][j] = 3;
         }
     }
+
     void gameOfLife(vector<vector<int>>& board) {
+
         for(int i=0;i<board.size();i++){
             for(int j=0;j<board[0].size();j++){
-                nextstate(board,i,j);
+                explore(board,i,j);
             }
         }
+        // printM(board);
 
         for(int i=0;i<board.size();i++){
             for(int j=0;j<board[0].size();j++){
                 if(board[i][j]==2){
-                    board[i][j]=1;
+                    board[i][j] = 0;
                 }
                 else if(board[i][j]==3){
-                    board[i][j]=0;
+                    board[i][j] = 1;
                 }
             }
         }
+
+
     }
 };
