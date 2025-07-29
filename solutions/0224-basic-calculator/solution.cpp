086e80cf58e92b37d2e58bb25aa55a709a789e77
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-07-29 15:46:35 +0000

    [LeetCode Sync] Runtime - 23 ms (7.24%), Memory - 24.9 MB (5.15%)

--- solutions/0224-basic-calculator/solution.cpp
+++ solutions/0224-basic-calculator/solution.cpp
@@ -1,35 +1,77 @@
 class Solution {
 public:
     int calculate(string s) {
-        long long int sum = 0;
-        int sign = 1;
-        stack<pair<int,int>> st;
-
-        for(int i=0; i<s.size();i++){
-            if(isdigit(s[i])){
-                long long int num = 0;
-                while(i<s.size() && isdigit(s[i])){
-                    num = num * 10 + (s[i] - '0');
+        stack<string> stk;
+        int i=0;
+        while(i<s.size()){
+            if(s[i]==' '){
+            }
+            else if(s[i]=='+'){
+                stk.push("+");
+            }
+            else if(s[i]=='-'){
+                stk.push("-");
+            }
+            else if((s[i]-'0'<=9) and (s[i]-'0'>=0)){
+                string curr = "";
+                while(i<s.size() and (s[i]-'0'<=9) and (s[i]-'0'>=0)){
+                    curr+= s[i];
                     i++;
                 }
+                stk.push(curr);
                 i--;
-                sum += num * sign;
-                sign = 1;
             }
-            else if(s[i] == '('){
-                st.push({sum, sign});
-                sum = 0;
-                sign = 1;
+            else if(s[i]=='('){
+                stk.push("(");
             }
-            else if(s[i] == ')'){
-                sum = st.top().first + (st.top().second * sum);
-                st.pop();
-
+            else if(s[i]==')'){
+                int result = 0;
+                while(stk.top()!="("){
+                    if(stk.top()=="+"){
+                        stk.pop();
+                    }
+                    int num = stoi(stk.top());
+                    stk.pop();
+                    if(stk.top()!="-"){
+                        num = num;
+                    }
+                    else{
+                        stk.pop();
+                        num = num*-1;
+                    }
+                    result+= num;
+                }
+                stk.pop();
+                stk.push(to_string(result));
             }
-            else if(s[i] == '-'){
-                sign = -1 * sign;
-            }
+            i++;
         }
-        return sum;
+
+        // string p =  "";
+        // while(!stk.empty()){
+        //    cout<<stk.top();
+        //     stk.pop();
+        // }
+       
+        int result = 0 ;
+                while(!stk.empty()){
+                    if(stk.top()=="+"){
+                        stk.pop();
+                    }
+                    int num = stoi(stk.top());
+                    stk.pop();
+                    if(!stk.empty()){
+                    if(stk.top()!="-"){
+                        num = num;
+                    }
+                    else{
+                        stk.pop();
+                        num = num*-1;
+                    }
+                    }
+                    result+= num;
+                }
+
+        return result;
     }
 };
