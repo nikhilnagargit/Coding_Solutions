7d85276ca3c760efe9a475f6b0f8b4245be4569e
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-07-29 13:33:42 +0000

    [LeetCode Sync] Runtime - 2 ms (71.36%), Memory - 10.8 MB (86.64%)

--- solutions/0071-simplify-path/solution.cpp
+++ solutions/0071-simplify-path/solution.cpp
@@ -1,47 +1,45 @@
-	
 class Solution {
 public:
     string simplifyPath(string path) {
-        
-        stack<string> st;
-        string res;
-        
-        for(int i = 0;  i<path.size(); ++i)
-        {
-            if(path[i] == '/')    
-                continue;
-            string temp;
-			// iterate till we doesn't traverse the whole string and doesn't encounter the last /
-            while(i < path.size() && path[i] != '/')
-            {
-				// add path to temp string
-                temp += path[i];
-                ++i;
+        string ans = "";
+        string curr = "";
+        vector<string> stack;
+        for(int i=0;i<path.size();i++){
+            if(path[i]=='/'){
+                if(curr==".."){
+                    if(stack.size()>=1)
+                    stack.pop_back();
+                }
+                else if(curr=="."){
+
+                }
+                else if(curr!=""){
+                    stack.push_back(curr);
+                }
+                curr="";
             }
-            if(temp == ".")
-                continue;
-			// pop the top element from stack if exists
-            else if(temp == "..")
-            {
-                if(!st.empty())
-                    st.pop();
+            else{
+                curr+= path[i];
             }
-            else
-			// push the directory file name to stack
-                st.push(temp);
         }
+
+       if(curr==".."){
+                    if(stack.size()>=1)
+                    stack.pop_back();
+                }
+                else if(curr=="."){
+
+                }
+                else if(curr!=""){
+                    stack.push_back(curr);
+                }
         
-		// adding all the stack elements to res
-        while(!st.empty())
-        {
-            res = "/" + st.top() + res;
-            st.pop();
+        for(auto s:stack){
+            // cout<<s<<endl;
+            ans+= '/'+s;
         }
-        
-		// if no directory or file is present
-        if(res.size() == 0)
-            return "/";
-        
-        return res;
+
+        if(ans=="") return "/";
+        return ans;
     }
 };
