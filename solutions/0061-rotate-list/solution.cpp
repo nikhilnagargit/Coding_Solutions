989d85582e4e9d3cef98507f51cbe0c0ce975eb2
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-07-30 17:10:33 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 16.5 MB (32.06%)

--- solutions/0061-rotate-list/solution.cpp
+++ solutions/0061-rotate-list/solution.cpp
@@ -1,24 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        if(head == NULL || k == 0 || head->next == NULL)    return head;
-
-        ListNode* curr = head;
-        int length = 1;
-
-        while(curr->next != NULL) {
-            curr = curr->next;
-            length++;
+        if(!head or k==0){
+            return head;
         }
 
-        curr->next = head;
-        k = k % length;
-        k = length - k;
-        while(k--)  curr = curr->next;
+        int size = 0;
+        ListNode* h = head;
+        while(h){
+            h=h->next;
+            size++;
+        }
+        int target = k%size;
 
-        head = curr->next;
-        curr->next = NULL;
+        ListNode* a = head;
+        ListNode* b = head;
 
+        while(target--){
+            b=b->next;
+        }
+
+        while(b->next){
+            b=b->next;
+            a=a->next;
+        }
+
+        if(a->next){
+        ListNode* temp = a->next;
+        a->next = nullptr;
+        b->next = head;
+        return temp;
+        }
         return head;
     }
 };
