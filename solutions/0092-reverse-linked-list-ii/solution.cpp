100c59791a59ded91b023040171be9141fc350df
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-07-30 16:03:05 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 11 MB (97.08%)

--- solutions/0092-reverse-linked-list-ii/solution.cpp
+++ solutions/0092-reverse-linked-list-ii/solution.cpp
@@ -9,83 +9,53 @@
  * };
  */
 class Solution {
-    ListNode* reverse(ListNode* head){
-        if(head==NULL || head->next==NULL)
-            return head;
-        ListNode*curr=head;
-        ListNode*prev=NULL;
-        ListNode*nex=NULL;
-        while(curr!=NULL){
-            nex=curr->next;
-            curr->next=prev;
-            prev=curr;
-            curr=nex;
+public:
+   ListNode* reverseLL(ListNode* head){
+        ListNode* first = head;
+        ListNode* second = head->next;
+        first->next = nullptr;
+        while(second){
+            ListNode* temp = second->next;
+            second->next = first;
+            first = second;
+            second = temp;
         }
-        return prev;
+        return first;
     }
 
-
-public:
     ListNode* reverseBetween(ListNode* head, int left, int right) {
-        if(head==nullptr or head->next ==nullptr or left==right){
-          return head;
-        }
-        ListNode* first_part;
-        if(left==1){
-          first_part = nullptr;
-        }else{
-          int i=2;
-          first_part = head;
-          while(i<left){
-            first_part=first_part->next;
+        ListNode *leftNode,*rightNode,*firstNode,*lastNode = nullptr;
+
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+
+        ListNode* curr = head;
+        ListNode* prev = dummy;
+
+        int i=1;
+        while(curr){
+            if(i==left){
+                leftNode = prev;
+                firstNode = curr;
+            }
+            if(i==right){
+                rightNode = curr->next;
+                lastNode = curr;
+            }
             i++;
-          }
-        }
-        int j=1;
-        ListNode* last_part;
-        last_part = head;
-        while(j<right){
-          last_part=last_part->next;
-          j++;
+            prev = curr;
+            curr = curr->next;
         }
 
-        if(last_part->next==nullptr){
-          last_part = nullptr;
-        }
-        else{
-          ListNode* tm = last_part;
-          last_part = last_part->next;
-          tm->next = nullptr;
-        }
+        lastNode->next = nullptr;
+        leftNode->next = nullptr;
+        ListNode* newHead = reverseLL(firstNode);
 
 
-        if(last_part!=nullptr and first_part!=nullptr){
-        // reverse middle lsit
-         ListNode* tem = first_part->next;
-          ListNode* start  = reverse(tem);
-        // attach in between
-          first_part->next =start;
-          tem->next = last_part;
-          return head;
-        }
-        else if(first_part!=nullptr){
-        // reverse middle lsit
-          ListNode* start  = reverse(first_part->next);
-        // attach in between
-          first_part->next =start;
-          return head;
-        }
-        else if(last_part!=nullptr){
-          ListNode* t = head;
-          head = reverse(head);
-          t->next = last_part;
-          return head;
-        }
-        else{
-          return reverse(head);
-        }
+        leftNode->next = lastNode;
+        if(firstNode)
+        firstNode->next= rightNode;
 
-return head;
-
+        return dummy->next;
     }
 };
