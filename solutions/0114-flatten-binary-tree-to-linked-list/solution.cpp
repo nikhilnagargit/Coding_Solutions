43f145c74f2ff8a932a0ab216459d7f3343d6b46
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-08-01 09:48:59 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.6 MB (24.40%)

--- solutions/0114-flatten-binary-tree-to-linked-list/solution.cpp
+++ solutions/0114-flatten-binary-tree-to-linked-list/solution.cpp
@@ -11,20 +11,36 @@
  */
 class Solution {
 public:
-    void flatten(TreeNode* root) {
-        TreeNode* curr = root;
-        TreeNode* p;
-        while(curr){
-            if(curr->left!=nullptr){
-                p = curr->left;
-                while(p->right!=nullptr){
-                    p = p->right;
-                }
-                p->right = curr->right;
-                curr->right = curr->left;
-                curr->left = nullptr;
-            }
-            curr = curr->right;
+    TreeNode* solve(TreeNode* root){
+        if(!root) return root;
+        
+        if(root->left and root->right){
+            TreeNode* left = root->left;
+            TreeNode* right = root->right;
+            TreeNode* rightend = solve(right);
+            TreeNode* leftend = solve(left);
+            root->right = left;
+            leftend->right = right;
+            root->left = nullptr;
+            return rightend;
         }
+
+        if(root->left){
+            root->right = root->left;
+            TreeNode* leftend = solve(root->left);
+            root->left = nullptr;
+            return leftend;
+        }
+
+        if(root->right){
+            TreeNode* rightend = solve(root->right);
+            return rightend;
+        }
+
+        return root;
+    }
+
+    void flatten(TreeNode* root) {
+        TreeNode* x =  solve(root);
     }
 };
