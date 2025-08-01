356d9b2da6081702d1583a261cfbb5e44f6a78f9
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-08-01 14:55:05 +0000

    [LeetCode Sync] Runtime - 3 ms (99.89%), Memory - 17.3 MB (64.91%)

--- solutions/0236-lowest-common-ancestor-of-a-binary-tree/solution.cpp
+++ solutions/0236-lowest-common-ancestor-of-a-binary-tree/solution.cpp
@@ -9,26 +9,12 @@
  */
 class Solution {
 public:
-    bool doescontainanyvalue(TreeNode* root,int p,int q){
-        if(!root)return false;
-        if(root->val==p or root->val==q){
-            return true;
-        }
-        if(root->left==nullptr and root->right==nullptr){
-            return false;
-        }
-         return doescontainanyvalue(root->left,p,q) or doescontainanyvalue(root->right,p,q);
-    }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(!root)return root;
-        if(root->val==p->val or root->val==q->val){
-            return root;
-        }
-        bool isright = doescontainanyvalue(root->right,p->val,q->val);
-        bool isleft = doescontainanyvalue(root->left,p->val,q->val);
-        if(isright and isleft) return root;
-        if(isright) return lowestCommonAncestor(root->right,p,q);
-        if(isleft) return lowestCommonAncestor(root->left,p,q);
-        return nullptr;
+        if(root==p or root==q or !root) return root;
+        TreeNode* left= lowestCommonAncestor(root->left,p,q);
+        TreeNode* right= lowestCommonAncestor(root->right,p,q);
+        if(!left) return right;
+        if(!right) return left;
+        return root;
     }
 };
