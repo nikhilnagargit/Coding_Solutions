86cdf9541d5d5ea2cad7cee4fcff7f25438e45f9
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-08-02 09:26:13 +0000

    [LeetCode Sync] Runtime - 21 ms (92.39%), Memory - 16.4 MB (66.73%)

--- solutions/0200-number-of-islands/solution.cpp
+++ solutions/0200-number-of-islands/solution.cpp
@@ -1,38 +1,28 @@
 class Solution {
 public:
-
-    void color(vector<vector<char>>& grid,int i,int j){
-        if(i>=grid.size() or i<0 or j<0 or j>=grid[0].size()){
+    void destroy(vector<vector<char>>&grid,int i,int j){
+        if(i<0 or i>=grid.size() or j<0 or j>=grid[0].size()){
             return;
         }
-        if(grid[i][j]=='0' or grid[i][j]=='2'){
+        if(grid[i][j]=='0'){
             return;
         }
-        if(grid[i][j]=='1'){
-            grid[i][j] = '2';
-        }
-        color(grid,i,j+1);
-        color(grid,i,j-1);
-        color(grid,i+1,j);
-        color(grid,i-1,j);
+        grid[i][j]='0';
+        destroy(grid,i+1,j);
+        destroy(grid,i,j+1);
+        destroy(grid,i-1,j);
+        destroy(grid,i,j-1);
     }
-
     int numIslands(vector<vector<char>>& grid) {
-        int count_of_color_initiation =0;
-
-        // find 1 and start coloring(negative 1)
-        int i=0;
-        int j=0;
-
+        int ans = 0;
         for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid[0].size();j++){
                 if(grid[i][j]=='1'){
-                    color(grid,i,j);
-                    count_of_color_initiation++;
+                    ans++;
+                    destroy(grid,i,j);
                 }
             }
         }
-
-        return count_of_color_initiation;
+        return ans;
     }
 };
