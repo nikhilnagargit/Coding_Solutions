4c3d8fefb45134200fadd718b6032e0564c55eb5
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-08-02 08:40:14 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 21.9 MB (73.83%)

--- solutions/0098-validate-binary-search-tree/solution.cpp
+++ solutions/0098-validate-binary-search-tree/solution.cpp
@@ -1,18 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    bool isValidBST(TreeNode* root) {
-        return bst(root, LLONG_MIN, LLONG_MAX);
+    long maxi(long a,long b){
+        if(a>b) return a;
+        return b;
+    }
+    long mini(long a,long b){
+        if(a>b) return b;
+        return a;
+    }
+    long find_biggest(TreeNode* root){
+        if(!root) return LONG_MIN;
+        return maxi(root->val,maxi(find_biggest(root->left),find_biggest(root->right)));
+    }
+    long find_smallest(TreeNode* root){
+        if(!root) return LONG_MAX;
+        return mini(root->val,mini(find_smallest(root->left),find_smallest(root->right)));
     }
 
-    bool bst(TreeNode* root, long long min_val, long long max_val) {
-        if (root == NULL) {
-            return true;
-        }
-
-        if (!(min_val < root->val && root->val < max_val)) {
-            return false;
-        }
-
-        return bst(root->left, min_val, root->val) && bst(root->right, root->val, max_val);
+    bool isValidBST(TreeNode* root) {
+        if(!root) return true;
+        long maxleft = find_biggest(root->left);
+        if(root->val <= maxleft) return false;
+        long minright = find_smallest(root->right);
+        if(root->val >= minright) return false;
+        return isValidBST(root->left) and isValidBST(root->right);
     }
 };
