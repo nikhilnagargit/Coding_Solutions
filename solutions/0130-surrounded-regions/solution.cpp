e6b0d2a0d9aeaa6dbb05b692d431525406de3e8a
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-08-02 09:52:37 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 13.7 MB (97.00%)

--- solutions/0130-surrounded-regions/solution.cpp
+++ solutions/0130-surrounded-regions/solution.cpp
@@ -1,60 +1,72 @@
 class Solution {
 public:
+    void printBoard(vector<vector<char>>& board){
+        int m = board[0].size();
+        int n = board.size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                cout<<board[i][j]<<",";
+            }
+            cout<<endl;
+        }
+        cout<<"------------------"<<endl;
+    }
+    void convertToY(vector<vector<char>>& board,int i,int j){
+        if(i<0 or i>=board.size() or j<0 or j>=board[0].size()){
+            return;
+        }
+        if(board[i][j]=='Y' or board[i][j]=='X'){
+            return;
+        }
+        board[i][j]='Y';
+        convertToY(board,i+1,j);
+        convertToY(board,i,j+1);
+        convertToY(board,i-1,j);
+        convertToY(board,i,j-1);
+    }
 
-   bool is_valid(vector<vector<char>>& board,int i,int j){
-       int m = board.size()-1;
-       int n = board[0].size()-1;
-       if(i>=0 and i<=m and j>=0 and j<=n){
-           return true;
-       }
-       return false;
-   }
-    void convert(vector<vector<char>>& board,char source,char target){
-        int m = board.size();
-        int n = board[0].size();
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(board[i][j]==source){
-                    board[i][j]=target;
+    void solve(vector<vector<char>>& board) {
+        int n = board.size();
+        int m = board[0].size();
+
+        //horizontal
+        for(int j=0;j<m;j++){
+            if(board[0][j]=='O'){
+                convertToY(board,0,j);
+            }
+            if(board[n-1][j]=='O'){
+                convertToY(board,n-1,j);
+            }
+        }
+
+        //vertical
+        for(int i=0;i<n;i++){
+            if(board[i][m-1]=='O'){
+                convertToY(board,i,m-1);
+            }
+            if(board[i][0]=='O'){
+                convertToY(board,i,0);
+            }
+        }
+        //  printBoard(board);
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j]=='O'){
+                    board[i][j] = 'X';
                 }
             }
         }
-    }
+        //  printBoard(board);
 
-   void make_T(vector<vector<char>>& board,int i,int j){
-       if(!is_valid(board,i,j)){
-           return;
-       }
-       if(board[i][j]=='O'){
-           board[i][j] = 'T';
-           make_T(board,i+1,j);
-           make_T(board,i,j+1);
-           make_T(board,i-1,j);
-           make_T(board,i,j-1);
-       }
-    }
- 
-
-    void solve(vector<vector<char>>& board) {
-        int m = board.size();
-        int n = board[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j]=='Y'){
+                    board[i][j] = 'O';
+                }
+            }
+        }
+        //  printBoard(board);
         
-
-        for(int j=0;j<n;j++){
-            // top boundary  i=0;
-            make_T(board,0,j);
-            // bottom boundary  i=m-1;
-            make_T(board,m-1,j);
-        }
-
-        for(int i=1;i<m-1;i++){
-            // right boundary j=n-1;
-            make_T(board,i,n-1);
-            // left boundary j=0;
-            make_T(board,i,0);
-        }
-        convert(board,'O','X');
-        convert(board,'T','O');
-
     }
 };
