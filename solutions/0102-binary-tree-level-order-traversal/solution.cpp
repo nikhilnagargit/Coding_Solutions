d61744d262acb4e9ca067559abd35b31a230fc54
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-08-02 07:52:09 +0000

    [LeetCode Sync] Runtime - 3 ms (28.02%), Memory - 17.2 MB (16.01%)

--- solutions/0102-binary-tree-level-order-traversal/solution.cpp
+++ solutions/0102-binary-tree-level-order-traversal/solution.cpp
@@ -1,25 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>> result;
-        int count = 0;
-        level(root , count , result);
-        return result;
+        if(!root){return vector<vector<int>>();}
+         queue<TreeNode*> q;
+        vector<vector<int>> ans;
+        q.push(root);
+        while(!q.empty()){
+            int size = q.size();
+            vector<int> v;
+            for(int i=0;i<size;i++){
+                TreeNode* curr = q.front();q.pop();
+                    v.push_back(curr->val);
+                                    if(curr->left){
+                    q.push(curr->left);
+                }
+                if(curr->right){
+                    q.push(curr->right);
+                }
 
+            }
+            ans.push_back(v);
+        }
+        return ans;
     }
-private:
-    void level(TreeNode* root , int count , vector<vector<int>> &result){
-        if(root == NULL){
-            return ;
-        }
-
-        if(result.size() <= count){
-            result.push_back(vector<int>());
-        }
-        result[count].push_back(root->val);
-        count++;
-        level(root->left , count , result);
-        level(root->right , count , result);
-  
-    }    
 };
