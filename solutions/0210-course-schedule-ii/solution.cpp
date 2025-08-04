59492c7e666b680879991bd387cdefa27e85eb89
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-08-04 15:39:05 +0000

    [LeetCode Sync] Runtime - 4 ms (55.67%), Memory - 18.6 MB (45.12%)

--- solutions/0210-course-schedule-ii/solution.cpp
+++ solutions/0210-course-schedule-ii/solution.cpp
@@ -1,29 +1,35 @@
 class Solution {
 public:
-        vector<int>ans;
-        unordered_set<int>ansset;
-    unordered_map<int,vector<int>>m;
-    unordered_set<int>vis;
-    bool dfs(int s){
-        if(vis.count(s)) return true;
-        vis.insert(s);
-        for(auto nb:m[s]){
-            if(dfs(nb))return true;
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> ans;
+        vector<int> inDegrees(numCourses,0);
+        unordered_map<int,vector<int>>m;
+        for(auto& p:prerequisites){
+            m[p[1]].push_back(p[0]);
+            inDegrees[p[0]]++;
         }
-        m.erase(s);
-        vis.erase(s);
-        if(ansset.find(s)==ansset.end()){
-        ans.push_back(s);
-        ansset.insert(s);
+        queue<int>q;
+        for(int i=0;i<numCourses;i++){
+            if(inDegrees[i]==0){
+                q.push(i);
+            }
         }
-        return false;
-    }
-    vector<int> findOrder(int n, vector<vector<int>>& prqs) {
-        for(auto prq:prqs){
-            m[prq[0]].push_back(prq[1]);
+        while(!q.empty()){
+            int s= q.size();
+            for(int i=0;i<s;i++){
+                int curr = q.front();q.pop();
+                ans.push_back(curr);
+                for(auto& n:m[curr]){
+                    inDegrees[n]--;
+                    if(inDegrees[n]==0){
+                        q.push(n);
+                    }
+                }
+            }
         }
-        for(int i=0;i<n;i++){
-            if(dfs(i))return vector<int>();
+
+        if(ans.size()!=numCourses){
+            return vector<int>();
         }
         return ans;
     }
