2cfd27aaf437a680995335de90bf9f2ab6daad9f
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-08-05 06:10:21 +0000

    [LeetCode Sync] Runtime - 7 ms (30.79%), Memory - 19.2 MB (15.74%)

--- solutions/0945-snakes-and-ladders/solution.cpp
+++ solutions/0945-snakes-and-ladders/solution.cpp
@@ -1,49 +1,53 @@
 class Solution {
 public:
     int snakesAndLadders(vector<vector<int>>& board) {
-        // flatten the array
-        vector<int> arr;
-        arr.push_back(0);
-        int n= board.size();
-        for(int i=0;i<n;i++){
-          for(int j=0;j<n;j++){
-            if(i%2==0){
-              arr.push_back(board[n-i-1][j]);
-            }else{
-              arr.push_back(board[n-i-1][n-j-1]);
-            }
-          }
-        }
-        for(auto i:arr){
-            cout<<i<<",";
-        }
-        cout<<endl;
-        // store minimum moves to reach current index, create a vector
-        vector<int> moves(n*n+1,-1);
-        moves[1]=0;
-        queue<int> q;
-        q.push(1);
-        while(!q.empty()){
-            int curr = q.front();
-            q.pop();
-            // try all possible moves from current index
-            for(int i=curr+1;i<=min(curr+6,n*n);i++){
-                int destination;
-                if(arr[i]==-1){
-                    destination = i;
+        vector<int> v;
+        v.push_back(0);
+        int m = board[0].size();
+        int n = board.size();
+
+        for(int i=0;i<board.size();i++){
+            for(int j=0;j<board[0].size();j++){
+                if(i%2==0){
+                    // left to right
+                    v.push_back(board[n-i-1][j]);
                 }
                 else{
-                    destination = arr[i];
-                }
-                if(moves[destination]==-1){
-                    moves[destination]=moves[curr]+1;
-                    q.push(destination);
+                    //right to left
+                    v.push_back(board[n-i-1][m-1-j]);
                 }
             }
         }
-        for(auto i:moves){
-            cout<<i<<",";
+
+        int N = n*m;
+
+        queue<int> q;
+        q.push(1);
+        unordered_set<int> visited;
+        int count = 0;
+        while(!q.empty()){
+            int  s = q.size();
+            for(int i=0;i<s;i++){
+                int curr = q.front();q.pop();
+                if(curr==N){
+                    return count;
+                }
+                if(visited.count(curr)){
+                    continue;
+                }
+                visited.insert(curr);
+                for(int j=1;j<=6;j++){
+                    int next = curr+j;
+                    if(next<v.size() and v[next]!=-1){
+                        next = v[next];
+                    }
+                    q.push(next);
+                }
+            }
+            count++;
         }
-        return moves[n*n];
+        
+        return -1;
+
     }
 };
