adf2e468c4171798c6cd7edfc20aa19eda758fad
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-08-05 05:24:34 +0000

    [LeetCode Sync] Runtime - 57 ms (60.02%), Memory - 19.6 MB (92.16%)

--- solutions/0127-word-ladder/solution.cpp
+++ solutions/0127-word-ladder/solution.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_map<string,int> mp;
-        for(string s: wordList){
-            mp[s]++;
-        }
-        queue<pair<string,int>> q;
-        q.push({beginWord,1});
-        if(mp[beginWord] > 0) mp[beginWord]--;
-
+        queue<string> q;
+        unordered_set<string> wordSet(wordList.begin(),wordList.end());
+        q.push(beginWord);
+        int sq = 1;
         while(!q.empty()){
-            string sf = q.front().first;
-            int d = q.front().second;
-            if(sf == endWord) return d;
-            q.pop();
-
-            for(int i = 0; i < sf.size();i++){
-                string t = sf;
-                for(int j = 0; j < 26; j++){
-                    t[i] = 'a' + j;
-                    if(mp[t] > 0){
-                        q.push({t,d+1});
-                        mp[t]--;
+            int s = q.size();
+            for(int i=0;i<s;i++){
+                string curr = q.front();q.pop();
+                for(int idx=0;idx<curr.size();idx++){
+                    string temp = curr;
+                    for(int c=0;c<=25;c++){
+                        temp[idx]='a'+c;
+                        if(wordSet.count(temp)){
+                            if(temp==endWord){
+                            return sq+1;
+                            }
+                            q.push(temp);
+                            wordSet.erase(temp);
+                        }
                     }
                 }
             }
+            sq++;
         }
-
         return 0;
     }
 };
