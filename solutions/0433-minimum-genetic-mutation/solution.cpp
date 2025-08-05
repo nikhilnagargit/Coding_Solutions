a8e18a2208a17987cf0cce8d2d4dc8b7bb7c9a5b
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-08-05 04:13:22 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.1 MB (16.28%)

--- solutions/0433-minimum-genetic-mutation/solution.cpp
+++ solutions/0433-minimum-genetic-mutation/solution.cpp
@@ -1,61 +1,64 @@
 class Solution {
-public:
-// check if strings only have one char difference
-    bool isOneMutation(const string& a,const string& b){
-        int count=0;
+public: 
+    unordered_map<string,vector<string>>m;
+    int min_path = INT_MAX;
+    bool isOneDiff(string a,string b){
+        int count = 0;
         for(int i=0;i<8;i++){
-            if(a[i]!=b[i])count++;
+            if(a[i]!=b[i]){
+                count++;
+            }
         }
-        if(count==1)return true;
+        if(count==1){
+            return true;
+        }
         return false;
+    }
+    
+    void findMinimumPath(string curr,string& target,unordered_set<string>&visited,int level){
+        if(curr==target){
+            min_path = min(min_path,level);
+            return;
+        }
+        if(visited.count(curr)){
+            return;
+        }
+        visited.insert(curr);
+        for(auto& neighbor:m[curr]){
+           findMinimumPath(neighbor,target,visited,level+1);
+        }
+        visited.erase(curr);
     }
 
     int minMutation(string startGene, string endGene, vector<string>& bank) {
-        // prepare the adjList using map(it will be added as bi-directional edges)
-        unordered_map<string,vector<string>>m;
+
+        auto it = find(bank.begin(),bank.end(),startGene);
+        if(it==bank.end()){
+            bank.push_back(startGene);
+        }
+
         for(int i=0;i<bank.size();i++){
             for(int j=i+1;j<bank.size();j++){
-                if(isOneMutation(bank[i],bank[j])){
+                if(isOneDiff(bank[i],bank[j])){
                     m[bank[i]].push_back(bank[j]);
                     m[bank[j]].push_back(bank[i]);
                 }
             }
         }
-        // check if endGene exist in bank or not
-        int flag = false;
-        for(int i=0;i<bank.size();i++){
-            if(isOneMutation(bank[i],startGene)){
-                m[startGene].push_back(bank[i]);
-                m[bank[i]].push_back(startGene);
-            }
-            // check if endgene exist or not
-            if(bank[i]==endGene){
-                flag = true;
-            }
-        }
-        if(!flag)return -1;
+        
 
-        // start traversing the graph bfs
-        queue<string>q;
-        unordered_set<string>vis;
-        q.push(startGene);
-        int count =0 ;
-        while(!q.empty()){
-            int size = q.size();
-            for(int i=0;i<size;i++){
-                string curr  = q.front();q.pop();
-                vis.insert(curr);
-                if(curr==endGene){
-                    return count;
-                }
-                for(auto item:m[curr]){
-                    if(vis.find(item)==vis.end()){
-                        q.push(item);
-                    }
-                }
-            }
-            count++;
+        unordered_set<string> visited;
+        // for(auto item:m){
+        //     cout<<item.first<<":{";
+        //     for(auto n:item.second){
+        //         cout<<n<<",";
+        //     }
+        //     cout<<"}, "<<endl;
+        // }
+        findMinimumPath(startGene,endGene,visited,0);
+        if(min_path==INT_MAX){
+            return -1;
         }
-        return -1;
+        return min_path;
     }
 };
