484e28e34403c15f613be69068de35a3c896f0e9
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-08-06 16:48:22 +0000

    [LeetCode Sync] Runtime - 4 ms (11.72%), Memory - 13.2 MB (5.99%)

--- solutions/0046-permutations/solution.cpp
+++ solutions/0046-permutations/solution.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
-    void permuteRec(vector<int>& nums, int begin, vector<vector<int>>& result) {
-        if (begin == nums.size()) {
-            result.push_back(nums);
+    vector<vector<int>>ans;
+     
+    void solve(unordered_set<int>& s, vector<int>& v){
+        if(s.size()==0){
+            ans.push_back(v);
             return;
         }
-        for (int i = begin; i < nums.size(); i++) {
-            // Swap
-            int temp = nums[begin];
-            nums[begin] = nums[i];
-            nums[i] = temp;
-            
-            permuteRec(nums, begin + 1, result);
-            
-            // Swap back
-            temp = nums[begin];
-            nums[begin] = nums[i];
-            nums[i] = temp;
+        unordered_set<int> s_copy = s;
+        for(auto item:s){
+            v.push_back(item);
+            s_copy.erase(item);
+            solve(s_copy,v);
+            s_copy.insert(item);
+            v.pop_back();
         }
+
     }
-    
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> result;
-        permuteRec(nums, 0, result);
-        return result;
+        vector<int>v;
+        unordered_set<int> s(nums.begin(),nums.end());
+        solve(s,v);
+        return ans;
     }
 };
