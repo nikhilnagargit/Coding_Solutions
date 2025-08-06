efaebad2eae38aa011272428be94fb8f0f84a1c3
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-08-06 15:51:51 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.7 MB (12.64%)

--- solutions/0017-letter-combinations-of-a-phone-number/solution.cpp
+++ solutions/0017-letter-combinations-of-a-phone-number/solution.cpp
@@ -1,42 +1,25 @@
 class Solution {
 public:
-    vector<string> mapping;
-    Solution(){
-        mapping = vector<string>(10,"");
-        mapping[2] = "abc"; 
-        mapping[3] = "def"; 
-        mapping[4] = "ghi"; 
-        mapping[5] = "jkl"; 
-        mapping[6] = "mno"; 
-        mapping[7] = "pqrs"; 
-        mapping[8] = "tuv"; 
-        mapping[9] = "wxyz"; 
-    }
-
-    void solve(string& digits,string& s,vector<string>&ans,int i){
-
-
-        if(s.size()==digits.size()){
-            ans.push_back(s);
+    vector<string> output;
+    unordered_map<char,vector<char>>m = {{'2',{'a','b','c'}},{'3',{'d','e','f'}},{'4',{'g','h','i'}},{'5',{'j','k','l'}},{'6',{'m','n','o'}},{'7',{'p','q','r','s'}},{'8',{'t','u','v'}},{'9',{'w','x','y','z'}}};
+    void solve(string& digits,int i,string& s){
+        if(i==digits.size()){
+            if(s!="")
+            output.push_back(s);
             return;
         }
-        string s_backup = s;
-        // cout<<int(digits[i]-'0');
-        string m = mapping[int(digits[i]-'0')];
-        for(int j=0;j<m.size();j++){
-            s+=m[j];
-            solve(digits,s,ans,i+1);
-            s=s_backup;
+        for(auto c:m[digits[i]]){
+            s+= c;
+            solve(digits,i+1,s);
+            s = s.substr(0,s.size()-1);
         }
-        return ;
+       
     }
     vector<string> letterCombinations(string digits) {
-        if(digits.size()==0){
-            return vector<string>(0);
-        }
-        vector<string>ans;
-        string s = "";
-        solve(digits,s,ans,0);
-        return ans;         
+
+         string s = "";
+        solve(digits,0,s);
+        return output;
     }
+    
 };
