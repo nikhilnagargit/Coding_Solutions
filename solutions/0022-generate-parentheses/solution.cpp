2cc829924a74b8292279104bb16ede1fa4e63dc8
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-08-08 16:13:24 +0000

    [LeetCode Sync] Runtime - 3 ms (69.79%), Memory - 16.2 MB (19.55%)

--- solutions/0022-generate-parentheses/solution.cpp
+++ solutions/0022-generate-parentheses/solution.cpp
@@ -1,44 +1,32 @@
 class Solution {
 public:
-    void solve(string op, int open, int close, vector<string> &ans){
-        if(open == 0 && close == 0){
-            ans.push_back(op);
+    vector<string>ans;
+
+    void backtracking(int& n,int& open,int& close,string& s){
+        if(open==close and open+close==2*n){
+            ans.push_back(s);
             return;
         }
-        //when count of open and close brackets are same then 
-        //we have only one choice to put open bracket 
-        if(open == close){
-            string op1 = op;
-            op1.push_back('(');
-            solve(op1, open-1, close, ans);
+        if(open>close){
+            s = s+")";
+            close = close+1;
+            backtracking(n,open,close,s);
+            close = close-1;
+            s.pop_back();
         }
-        else if(open == 0){
-            //only choice is to put close brackets 
-            string op1 = op;
-            op1.push_back(')');
-            solve(op1, open, close-1, ans);
-        }
-        else if(close == 0){
-            //only choise is to use open bracket 
-            string op1 = op;
-            op1.push_back('(');
-            solve(op1, open-1, close, ans);
-        }
-        else{
-            string op1 = op;
-            string op2 = op;
-            op1.push_back('(');
-            op2.push_back(')');
-            solve(op1, open-1, close, ans);
-            solve(op2, open, close-1, ans);
+        if(open<n){
+            open = open+1;
+            s+="(";
+            backtracking(n,open,close,s);
+            open = open-1;
+            s.pop_back();
         }
     }
     vector<string> generateParenthesis(int n) {
-        int open = n;
-        int close = n;
-        vector<string> ans;
-        string op = "";
-        solve(op, open, close, ans);
+        string s= "";
+        int open = 0;
+        int close = 0;
+        backtracking(n,open,close,s);
         return ans;
     }
 };
