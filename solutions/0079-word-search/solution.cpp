35f1db7bb325563729dffa2d1c072f57622c040f
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-08-08 16:23:45 +0000

    [LeetCode Sync] Runtime - 250 ms (56.36%), Memory - 10.5 MB (67.37%)

--- solutions/0079-word-search/solution.cpp
+++ solutions/0079-word-search/solution.cpp
@@ -1,25 +1,33 @@
 class Solution {
 public:
-    bool solve(vector<vector<char>>& board,string word,int i,int j){
-        if(word=="")return true;
-        if(i<0 or j<0 or i>=board.size() or j>=board[0].size() or board[i][j]=='#')return false;
-        char c = board[i][j];
-        if(word[0]==c){
-            board[i][j] = '#';
-            if(solve(board,word.substr(1),i+1,j) or 
-            solve(board,word.substr(1),i-1,j) or 
-            solve(board,word.substr(1),i,j+1) or
-            solve(board,word.substr(1),i,j-1))return true;
-            board[i][j] = c;
+    bool dfs(vector<vector<char>>& board, string& word,int idx,int i,int j,vector<vector<int>>& visited){
+                if(idx==word.size()){
+            return true;
         }
+        if(i<0 or j<0 or i>=board.size() or j>=board[0].size()){
+            return false;
+        }
+
+        if(visited[i][j]){
+            return false;
+        }
+        if(word[idx]!=board[i][j])return false;
+
+        visited[i][j]=1;
+        if(dfs(board,word,idx+1,i+1,j,visited)) return true;
+        if(dfs(board,word,idx+1,i,j+1,visited)) return true;
+        if(dfs(board,word,idx+1,i-1,j,visited))return true;
+        if(dfs(board,word,idx+1,i,j-1,visited))return true;
+        visited[i][j]=0;
         return false;
     }
     bool exist(vector<vector<char>>& board, string word) {
+        vector<vector<int>> visited(board.size(),vector<int>(board[0].size(),0));
         for(int i=0;i<board.size();i++){
             for(int j=0;j<board[0].size();j++){
-                if(solve(board,word,i,j))return true;
+                if(dfs(board,word,0,i,j,visited)) return true;
             }
         }
-    return false;    
+        return false;
     }
 };
