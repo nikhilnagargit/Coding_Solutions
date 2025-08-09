9e6d39c2e9768bbf8a01e9b1347ce998cf876ce1
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-08-09 05:10:55 +0000

    [LeetCode Sync] Runtime - 263 ms (5.01%), Memory - 18.7 MB (37.96%)

--- solutions/0023-merge-k-sorted-lists/solution.cpp
+++ solutions/0023-merge-k-sorted-lists/solution.cpp
@@ -1,42 +1,56 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        if (lists.empty()) {
-            return nullptr;
-        }
-        return mergeKListsHelper(lists, 0, lists.size() - 1);
+    bool operator()(ListNode* a, ListNode* b) const {
+        // return true if a should come after b, i.e. a.val > b.val
+        if(a and b)
+        return a->val > b->val;
+        return true;
     }
-    
-    ListNode* mergeKListsHelper(vector<ListNode*>& lists, int start, int end) {
-        if (start == end) {
-            return lists[start];
-        }
-        if (start + 1 == end) {
-            return merge(lists[start], lists[end]);
-        }
-        int mid = start + (end - start) / 2;
-        ListNode* left = mergeKListsHelper(lists, start, mid);
-        ListNode* right = mergeKListsHelper(lists, mid + 1, end);
-        return merge(left, right);
-    }
-    
-    ListNode* merge(ListNode* l1, ListNode* l2) {
-        ListNode* dummy = new ListNode(0);
-        ListNode* curr = dummy;
-        
-        while (l1 && l2) {
-            if (l1->val < l2->val) {
-                curr->next = l1;
+    ListNode* merge(ListNode* l1,ListNode* l2){
+        ListNode* temp = new ListNode();
+        ListNode* dummy = temp;
+        while(l1 and l2){
+            if(l1->val<l2->val){
+                dummy->next = l1;
                 l1 = l1->next;
-            } else {
-                curr->next = l2;
+            }
+            else{
+                dummy->next = l2;
                 l2 = l2->next;
             }
-            curr = curr->next;
+            dummy=dummy->next;
         }
-        
-        curr->next = l1 ? l1 : l2;
-        
-        return dummy->next;
+        if(l1){
+            dummy->next = l1;
+        }
+        if(l2){
+            dummy->next = l2;
+        }
+        return temp->next;
+    }
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if(lists.size()==0){
+            return nullptr;
+        }
+        priority_queue<ListNode*,vector<ListNode*>,Solution> heap;
+        for(int i=0;i<lists.size();i++){
+            heap.push(lists[i]);
+        }
+        while(heap.size()>1){
+            ListNode* a = heap.top();heap.pop();
+            ListNode* b = heap.top();heap.pop();
+            heap.push(merge(a,b));
+        }
+        return heap.top();
     }
 };
