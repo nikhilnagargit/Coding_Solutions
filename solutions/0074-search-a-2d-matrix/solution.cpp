5e863f5331547aabfbeed5e565321ac46ca33724
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-08-09 07:15:40 +0000

    [LeetCode Sync] Runtime - 3 ms (1.26%), Memory - 13.5 MB (7.04%)

--- solutions/0074-search-a-2d-matrix/solution.cpp
+++ solutions/0074-search-a-2d-matrix/solution.cpp
@@ -1,39 +1,41 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-        int topRow = 0;
-        int bottomRow = n-1;
-        while(topRow<=bottomRow){
-            int midRow = topRow + (bottomRow-topRow)/2;
-            if(target>= matrix[midRow][0] and target<=matrix[midRow][m-1]){
-                // start another binary search
-                int left = 0;
-                int right = m-1;
-                while(left<=right){
-                    int mid = left+ (right-left)/2;
-                    if(matrix[midRow][mid]==target){
-                        return true;
-                    }
-                    else if(matrix[midRow][mid]>target){
-                        right = mid-1;
-                    }
-                    else{
-                        left = mid+1;
-                    }
-                }
-                return false;
+        // find row from first column
+        int top = 0;
+        int bottom = matrix.size()-1;
+        while(top<bottom){
+            int mid = top+(bottom-top)/2;
+            if(matrix[mid][0]==target){
+                return true;
             }
-            else if(target>matrix[midRow][m-1]){
-                topRow = midRow+1;
+            else if(matrix[mid][0]>target){
+                bottom = mid-1;
+            }
+            else if(matrix[mid][0]<target and matrix[mid][matrix[0].size()-1]>=target) {
+                bottom = mid;
+                top = mid;
             }
             else{
-                bottomRow = midRow-1;
+                top = mid+1;
             }
-
         }
-    return false;
-
+        cout<<top;
+        int left = 0;
+        int right = matrix[0].size()-1;
+        while(left<=right){
+             int mid = left+(right-left)/2;
+            if(matrix[top][mid]==target){
+                return true;
+            }
+            else if(matrix[top][mid]>target){
+                right = mid-1;
+            }
+            else{
+                left = mid+1;
+            }
+        }
+        
+        return false;
     }
 };
