9645408ed3ffea2dca8af59e98faac3a6e6dd206
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-08-09 13:53:04 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 15.1 MB (66.76%)

--- solutions/0033-search-in-rotated-sorted-array/solution.cpp
+++ solutions/0033-search-in-rotated-sorted-array/solution.cpp
@@ -1,57 +1,44 @@
 class Solution {
 public:
-    int binary_search(vector<int>& nums,int target,int left,int right){
-        if(nums[0]==target)return 0;
-        
+    int binarySearch(vector<int>& nums,int target,int left,int right){
         while(left<=right){
-            int mid = left + (right-left)/2;
-            if(nums[mid]==target)return mid;
-            else if(nums[mid]<target){
+            int mid = left +(right-left)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]>target){
+                right = mid-1;
+            }
+            else{
                 left = mid+1;
             }
-            else right = mid-1;
         }
         return -1;
     }
 
     int search(vector<int>& nums, int target) {
-        if(nums.size()==1 and nums[0]==target)return 0;
-        if(nums.size()==1 and nums[0]!=target)return -1;
-        
-
-        int left = 1;
-        int right = nums.size()-2;
-        int mid;
-        if(nums[0]>nums[1]){
-            mid = 0;
-        }
-        else if(nums[nums.size()-1]<nums[nums.size()-2]){
-            mid = nums.size()-2;
-        }
-        else{
-        while(left<=right){
-            mid = left+(right-left)/2;
-            if(nums[mid]>nums[mid+1]){
-                break;
-            }
-            else if(nums[mid]>nums[0]){
+        int left = 0;
+        int right = nums.size()-1;
+        // first find the peak
+        while(left<right){
+            int mid = left + (right-left)/2;
+            // left side sorted
+            if(nums[mid]>nums[right]){
                 left = mid+1;
             }
-            else if(nums[mid]<nums[0]){
-                right = mid-1;
-            }
+            //right side sorted
             else{
-                break;
+                right = mid;
             }
         }
-        }
-
-        int first = binary_search(nums,target,0,mid);
+        int peak = left;
+        cout<<peak;
+        int first = binarySearch(nums,target,0,peak-1);
         if(first!=-1) return first;
-        if(mid+1<=nums.size()-1){        
-        int second = binary_search(nums,target,mid+1,nums.size()-1);
-        return second;
-        }
+
+        int second = binarySearch(nums,target,peak,nums.size()-1);
+        if(second!=-1) return second;
+
         return -1;
     }
 };
