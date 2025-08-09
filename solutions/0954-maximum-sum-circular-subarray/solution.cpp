95f1e07dbde6895480f7882e66c68db207ac1b23
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-08-09 05:50:23 +0000

    [LeetCode Sync] Runtime - 1 ms (25.20%), Memory - 43.5 MB (76.98%)

--- solutions/0954-maximum-sum-circular-subarray/solution.cpp
+++ solutions/0954-maximum-sum-circular-subarray/solution.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
     int maxSubarraySumCircular(vector<int>& nums) {
-        int curMax = 0;
-        int curMin = 0;
-        int maxSum = nums[0];
-        int minSum = nums[0];
-        int totalSum = 0;
-        
-        for (int num: nums) {
-            // Normal Kadane's
-            curMax = max(curMax, 0) + num;
-            maxSum = max(maxSum, curMax);
-            
-            // Kadane's but with min to find minimum subarray
-            curMin = min(curMin, 0) + num;
-            minSum = min(minSum, curMin);
-            
-            totalSum += num;  
+        int max_sum = INT_MIN;
+        int sum=0;
+        for(int i=0;i<nums.size();i++){
+            if(sum<0){
+                sum=0;
+            }
+            sum+= nums[i];
+            max_sum = max(max_sum,sum);
         }
-
-        if (totalSum == minSum) {
-            return maxSum;
+        int max_sum1 = max_sum;
+        max_sum=INT_MIN;
+        sum=0;
+        int total=0;
+        for(int i=0;i<nums.size();i++){
+            if(sum<0){
+                sum=0;
+            }
+            sum+= -nums[i];
+            total+= -nums[i];
+            max_sum = max(max_sum,sum);
         }
-        
-        return max(maxSum, totalSum - minSum);
+        int max_sum2 = max_sum;
+        if(max_sum2==total){
+            return max_sum1;
+        }
+        return max(max_sum1,max_sum2-total);
     }
 };
