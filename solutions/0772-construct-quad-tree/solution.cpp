27afb60a3a1dc9e2812a7d3d25db6d4f885d8369
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-08-09 04:23:41 +0000

    [LeetCode Sync] Runtime - 18 ms (11.39%), Memory - 24.1 MB (9.47%)

--- solutions/0772-construct-quad-tree/solution.cpp
+++ solutions/0772-construct-quad-tree/solution.cpp
@@ -40,36 +40,27 @@ public:
 
 class Solution {
 public:
-
-    Node* solve(vector<vector<int>>&grid,int I,int J,int N,int M){
-        cout<<"I,J,N,M"<<I<<","<<J<<","<<N<<","<<M<<endl;
-        // check if all are one or all are zero
-        Node* node;
-        int val = grid[I][J];
-        bool allSame = true;
-        for(int i=I;i<=N;i++ ){
-            for(int j=J;j<=M;j++){
-                if(grid[i][j]!=val){
-                    allSame = false;
-                    break;
-                }
-            }if(!allSame) break;
+    Node* makeQuadTree(vector<vector<int>>& grid,int left,int right,int top,int bottom ){
+        if(left==right and top==bottom){
+            return new Node(grid[top][left],1);
         }
-
-        if(allSame){
-            node = new Node(val,true);
+        Node* root = new Node(1,0);
+        int xmid = left+(right-left)/2;
+        int ymid = top+(bottom-top)/2;
+        root->topLeft = makeQuadTree(grid,left,xmid,top,ymid);
+        root->topRight = makeQuadTree(grid,xmid+1,right,top,ymid);
+        root->bottomLeft = makeQuadTree(grid,left,xmid,ymid+1,bottom);
+        root->bottomRight = makeQuadTree(grid,xmid+1,right,ymid+1,bottom);
+        if(root->topLeft->val==root->topRight->val and root->bottomLeft->val==root->bottomRight->val and root->topLeft->val==root->bottomLeft->val and root->topLeft->isLeaf and root->topRight->isLeaf and root->bottomLeft->isLeaf and root->bottomRight->isLeaf){
+            if(root->topLeft->val==0){
+                return new Node(0,1);
+            }
+            return new Node(1,1);
         }
-        else{
-            node = new Node(1,false);
-            node->topLeft =  solve(grid,I,J,I+(N-I)/2,J+(M-J)/2);
-            node->topRight = solve(grid,I,J+(M-J)/2+1,I+(N-I)/2,M);
-            node->bottomLeft = solve(grid,I+(N-I)/2+1,J,N,J+(M-J)/2);
-            node->bottomRight = solve(grid,I+(N-I)/2+1,J+(M-J)/2+1,N,M);
-        }
-        return node;
+        return root;
     }
     Node* construct(vector<vector<int>>& grid) {
         int n = grid.size();
-        return solve(grid,0,0,n-1,n-1);
+        return makeQuadTree(grid,0,n-1,0,n-1);
     }
 };
