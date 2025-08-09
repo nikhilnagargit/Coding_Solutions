225d4fb2d3421e3ed6509d5651fee1fb1f2e7537
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-08-09 19:07:03 +0000

    [LeetCode Sync] Runtime - 4 ms (19.43%), Memory - 95.1 MB (79.16%)

--- solutions/0004-median-of-two-sorted-arrays/solution.cpp
+++ solutions/0004-median-of-two-sorted-arrays/solution.cpp
@@ -1,39 +1,43 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        if(nums1.size()>nums2.size()){
-            return findMedianSortedArrays(nums2,nums1);
+        // Ensure nums1 is the smaller array
+        if (nums1.size() > nums2.size()) {
+            return findMedianSortedArrays(nums2, nums1);
         }
 
-        int n1 = nums1.size();
-        int n2 = nums2.size();
-        int n = n1+n2;
-        int left = (n1+n2+1)/2;
-        int low = 0;
-        int high = n1;
-        while(low<=high){
-            int mid1 = (low+high)/2;
-            int mid2 = left-mid1;
-            int l1 = INT_MIN,l2 = INT_MIN, r1=INT_MAX, r2=INT_MAX;
-            if(mid1-1>=0)l1 = nums1[mid1-1];
-            if(mid2-1>=0)l2 = nums2[mid2-1];
-            if(mid1<n1) r1 = nums1[mid1];
-            if(mid2<n2) r2 = nums2[mid2];
-            if(l1<=r2 and l2<=r1){
-                if(n%2==1){
-                    return max(l1,l2);
+        int m = nums1.size();
+        int n = nums2.size();
+        int total = m + n;
+        int half = total / 2;
+
+        int left = 0;
+        int right = m;
+
+        while (left <= right) {
+            int midA = left + (right - left) / 2;
+            int midB = half - midA;
+
+            int leftA = (midA > 0) ? nums1[midA - 1] : INT_MIN;
+            int rightA = (midA < m) ? nums1[midA] : INT_MAX;
+
+            int leftB = (midB > 0) ? nums2[midB - 1] : INT_MIN;
+            int rightB = (midB < n) ? nums2[midB] : INT_MAX;
+
+            if (leftA <= rightB && leftB <= rightA) {
+                if (total % 2 == 0) {
+                    return (max(leftA, leftB) + min(rightA, rightB)) / 2.0;
                 }
-                else{
-                    return (double)(max(l1,l2)+min(r1,r2))/2.0;
-                }
-            }
-            else if(l1>r2){
-                high = mid1-1;
-            }
-            else{
-                low = mid1+1;
+                return min(rightA, rightB);
+            } else if (leftA > rightB) {
+                right = midA - 1;
+            } else {
+                left = midA + 1;
             }
         }
-        return 0;
+
+        // Should never reach here if inputs are valid
+        return 0.0;
     }
 };
+
