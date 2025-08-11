429b36a98a4dea3ebb1906022f92b17c5199eaad
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-08-11 16:33:13 +0000

    [LeetCode Sync] Runtime - 94 ms (29.77%), Memory - 148.5 MB (61.73%)

--- solutions/0295-find-median-from-data-stream/solution.cpp
+++ solutions/0295-find-median-from-data-stream/solution.cpp
@@ -1,59 +1,21 @@
 class MedianFinder {
 public:
-    priority_queue<int> maxheap;
-    priority_queue<int,vector<int>,greater<int>> minheap;
-
+    priority_queue<int> maxHeap;
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+    
     MedianFinder() {
     }
-    
     void addNum(int num) {
-            // if maxheap heap is empty
-            if(maxheap.size()==0){
-                maxheap.push(num);
-                return;
-            }
-            // if both heaps have same size
-            if(maxheap.size()==minheap.size()){
-                if(minheap.top()>num){
-                    maxheap.push(num);
-                    return;
-                }
-                else{
-                    maxheap.push(minheap.top());
-                    minheap.pop();
-                    minheap.push(num);
-                    return;
-                }
-            }
-            // if first heap is bigger
-            if(maxheap.top()>num){
-                minheap.push(maxheap.top());
-                maxheap.pop();
-                maxheap.push(num);
-                return;
-            }
-            else{
-                minheap.push(num);
-                return;
-            }
-
-
-        }
-    
-    
-    double findMedian() {
-        if(minheap.size()==maxheap.size()){
-            return (minheap.top()+maxheap.top())/2.0;
-        }
-        else{
-            return maxheap.top();
+        maxHeap.push(num);
+        minHeap.push(maxHeap.top());
+        maxHeap.pop();
+        if (minHeap.size() > maxHeap.size()) {
+            maxHeap.push(minHeap.top());
+            minHeap.pop();
         }
     }
+    double findMedian() {
+        if (maxHeap.size() > minHeap.size()) return maxHeap.top();
+        return (maxHeap.top() + minHeap.top()) / 2.0;
+    }
 };
-
-/**
- * Your MedianFinder object will be instantiated and called as such:
- * MedianFinder* obj = new MedianFinder();
- * obj->addNum(num);
- * double param_2 = obj->findMedian();
- */
