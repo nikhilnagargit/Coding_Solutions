69fc5e4d3ce494110e761a5b032f228219de1333
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-08-11 05:20:06 +0000

    [LeetCode Sync] Runtime - 108 ms (20.07%), Memory - 135.8 MB (9.63%)

--- solutions/0502-ipo/solution.cpp
+++ solutions/0502-ipo/solution.cpp
@@ -1,49 +1,40 @@
+class Cmp{
+    public:
+    bool operator()(pair<int,int>& a,pair<int,int>&b)const{
+        if(a.first > b.first)
+        return true;
+        if(a.first==b.first){
+            return a.second<b.second;
+        }
+        return false;
+    }
+};
+class Cmp2{
+    public:
+    bool operator()(pair<int,int>& a,pair<int,int>&b)const{
+        return a.second < b.second;
+    }
+};
+
 class Solution {
 public:
     int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
-        vector<pair<int,int>> v;
-        
+        priority_queue<pair<int,int>,vector<pair<int,int>>,Cmp> min_heap;
         for(int i=0;i<profits.size();i++){
-            v.push_back({capital[i],profits[i]});
+            min_heap.push({capital[i],profits[i]});
         }
-
-        sort(v.begin(),v.end());
-
-        int mycapital  = w;
-        int count = 0;
-        priority_queue<int> pq;
-        int i=0;
-        while(i<v.size()){
-            if(v[i].first<=mycapital){
-                cout<<"pushing"<<v[i].second<<" ";
-                pq.push(v[i].second);
-                i++;
-                continue;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,Cmp2> max_heap;
+        while((!min_heap.empty() or !max_heap.empty()) and k>0){
+            while(!min_heap.empty() and min_heap.top().first<=w){
+                max_heap.push(min_heap.top());
+                min_heap.pop();
             }
-            if(!pq.empty()){
-                mycapital += pq.top();pq.pop();
-                count++;
-                if(count==k){
-                    return mycapital;
-                }
-            }
-                   else{
-                return mycapital;
-            }
+            if(max_heap.empty()) break;
+            //select one with max profit from max heap
+            w+= max_heap.top().second;
+            max_heap.pop();
+            k--;
         }
-
-         while(true){
-             if(!pq.empty()){
-                mycapital += pq.top();pq.pop();
-                count++;
-                if(count==k){
-                    return mycapital;
-                }
-             }
-            else{
-                return mycapital;
-            }
-         }
-        return mycapital;
+        return w;
     }
 };
