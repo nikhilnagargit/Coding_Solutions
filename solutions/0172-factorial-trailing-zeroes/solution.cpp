bb4bef9fa83c1fdb29c4cc6ff03ea052ee468483
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-08-12 08:13:42 +0000

    [LeetCode Sync] Runtime - 2 ms (8.64%), Memory - 8.1 MB (50.39%)

--- solutions/0172-factorial-trailing-zeroes/solution.cpp
+++ solutions/0172-factorial-trailing-zeroes/solution.cpp
@@ -1,12 +1,28 @@
 class Solution {
 public:
-    int trailingZeroes(int n) {
-        int cnt=0;
-        while(n/5>0){
-            cnt=cnt+n/5;
-            n=n/5;
+    int find5factorcount(int n){
+        int count = 0;
+        while(n%5==0 and n!=0){
+            n = n/5;
+            count++;
         }
-
-        return cnt;
+        return count;
+    }
+    int find2factorcount(int n){
+        int count = 0;
+        while(n%2==0 and n!=0){
+            n = n/2;
+            count++;
+        }
+        return count;
+    }
+    int trailingZeroes(int n) {
+        int ans = 0;
+        int a = 0;
+        int b = 0;
+        for(int i=n;i>=1;i--){
+            a +=find5factorcount(i);
+        }
+        return a;
     }
 };
