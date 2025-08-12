cfb9f16941955692e005a3ef79b117bb9b558db4
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-08-12 05:34:29 +0000

    [LeetCode Sync] Runtime - 4 ms (14.18%), Memory - 13.2 MB (98.91%)

--- solutions/0137-single-number-ii/solution.cpp
+++ solutions/0137-single-number-ii/solution.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int ans = 0 ;
-        int countone = 0;
+        int ans = 0;
         for(int i=0;i<32;i++){
             int mask = 1<<i;
-            for(int j=0;j<nums.size();j++){
-                if((nums[j]&mask)!=0){
-                    countone++;
+            int ones = 0;
+            for(auto n:nums){
+                if(n&mask){
+                    ones+=1;
                 }
             }
-            if(countone%3!=0){
-                ans=ans|mask;
+            if(ones%3==1){
+                ans = ans|mask;
             }
-            countone = 0;
         }
         return ans;
     }
