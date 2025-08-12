c03c3730d16102e163fd6616dd700ee4ea5cd11e
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-08-12 06:57:55 +0000

    [LeetCode Sync] Runtime - 439 ms (5.04%), Memory - 11.4 MB (9.44%)

--- solutions/0201-bitwise-and-of-numbers-range/solution.cpp
+++ solutions/0201-bitwise-and-of-numbers-range/solution.cpp
@@ -1,14 +1,15 @@
-
 class Solution {
 public:
+
     int rangeBitwiseAnd(int left, int right) {
-        int cnt = 0;
-        while (left != right) {
-            left >>= 1;
-            right >>= 1;
-            cnt++;
+        if(left==0 or right==0) return 0;
+        int a = (int)log2(left);
+        int b = (int)log2(right);
+        if(a!=b) return 0;
+        long ans = left;
+        for(long i=left;i<=right;i++){
+            ans = ans&i;
         }
-        return (left << cnt);
+        return ans;
     }
 };
-
