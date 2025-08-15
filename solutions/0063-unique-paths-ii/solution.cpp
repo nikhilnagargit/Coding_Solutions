0509e51ba61d3c0764937f33cde56a390f676be7
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-08-15 06:59:39 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 11.9 MB (28.97%)

--- solutions/0063-unique-paths-ii/solution.cpp
+++ solutions/0063-unique-paths-ii/solution.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
-    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int rows = obstacleGrid.size();
-        int columns = obstacleGrid[0].size();
-
-        vector<vector<int>>dp(rows,vector<int>(columns,0));
-
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<columns;j++){
-                if(i==0 and j==0 and obstacleGrid[i][i]!=1){
-                    dp[i][j]=1;
+    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+         vector<vector<int>> dp(grid.size(),vector<int>(grid[0].size(),0));
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(i==0 and j==0){
+                    if(grid[i][j]==1) dp[i][j]=0;
+                    else dp[i][j]= 1;
                 }
-                else if(i==0 and obstacleGrid[i][j]!=1){
-                    dp[i][j]=dp[i][j-1];
+                else if(i==0){
+                    if(grid[i][j]==1) dp[i][j]=0;
+                    else dp[i][j]= dp[i][j-1];
                 }
-                else if(j==0 and obstacleGrid[i][j]!=1){
-                    dp[i][j]=dp[i-1][j];
-                }
-                else if(obstacleGrid[i][j]!=1){
-                    dp[i][j] = dp[i-1][j]+dp[i][j-1];
+                else if(j==0){
+                    if(grid[i][j]==1) dp[i][j]=0;
+                    else dp[i][j]= dp[i-1][j];
                 }
                 else{
-                    dp[i][j]=0;
+                    if(grid[i][j]==1) dp[i][j]=0;
+                    else dp[i][j]= dp[i-1][j]+dp[i][j-1];
                 }
             }
         }
-        return dp[rows-1][columns-1];
+        return dp[grid.size()-1][grid[0].size()-1];
+
     }
 };
