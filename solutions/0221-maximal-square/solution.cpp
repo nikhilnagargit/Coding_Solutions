476ad577d0063bca1153fec44a376890121f30c9
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-08-16 11:28:47 +0000

    [LeetCode Sync] Runtime - 3 ms (93.20%), Memory - 30.9 MB (62.14%)

--- solutions/0221-maximal-square/solution.cpp
+++ solutions/0221-maximal-square/solution.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
     int maximalSquare(vector<vector<char>>& matrix) {
-        if (matrix.empty()) {
-            return 0;
+        vector<vector<int>> dp(matrix.size(),vector<int>(matrix[0].size(),0));
+        int maxi = 0;
+        for(int i=0;i<matrix.size();i++){
+            dp[i][0]=0;
+            if(matrix[i][0]=='1')
+            dp[i][0]=1;
+            maxi = max(maxi,dp[i][0]);
         }
-        int m = matrix.size(), n = matrix[0].size(), sz = 0, pre;
-        vector<int> cur(n, 0);
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                int temp = cur[j];
-                if (!i || !j || matrix[i][j] == '0') {
-                    cur[j] = matrix[i][j] - '0';
-                } else {
-                    cur[j] = min(pre, min(cur[j], cur[j - 1])) + 1;
+        for(int j=0;j<matrix[0].size();j++){
+            dp[0][j]=0;
+            if(matrix[0][j]=='1')
+            dp[0][j]=1;
+            maxi = max(maxi,dp[0][j]);
+        }
+        for(int i=1;i<matrix.size();i++){
+            for(int j=1;j<matrix[0].size();j++){
+                if(matrix[i][j]=='1'){
+                    dp[i][j] = 1 + min({dp[i-1][j],dp[i-1][j-1],dp[i][j-1]});
                 }
-                sz = max(cur[j], sz);
-                pre = temp;
+                else{
+                    dp[i][j] = 0;
+                }
+                maxi = max(maxi,dp[i][j]);
             }
         }
-        return sz * sz;
+        return maxi*maxi;
     }
 };
