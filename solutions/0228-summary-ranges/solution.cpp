ccb53cf9170ea1eca84fb4927ca65af2cdfd9ba0
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-08-19 05:13:41 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.2 MB (42.10%)

--- solutions/0228-summary-ranges/solution.cpp
+++ solutions/0228-summary-ranges/solution.cpp
@@ -1,35 +1,22 @@
 class Solution {
 public:
     vector<string> summaryRanges(vector<int>& nums) {
-         vector<string> ans;
-        if(nums.size()==0){
-          return ans;
-        }
-        int start=nums[0];
-        int end = nums[0];
-        int i=1;
-   
-        while(i<nums.size()){
-            if(nums[i]-1==end){
-                end = nums[i];
-            }
-            else{
-                // print previoud range
-                if(start==end){
-                    ans.push_back(to_string(start));
-                }
+        vector<string> ans;
+        if(nums.size()==0) return ans;
+        int prev = nums[0];
+        for(int i=1;i<nums.size();i++){
+            if(nums[i-1]+1 != nums[i]){
+                if(prev==nums[i-1])
+                    ans.push_back(to_string(prev));
                 else
-                ans.push_back(to_string(start)+"->"+to_string(end));
-                start = nums[i];
-                end = nums[i];
+                ans.push_back(to_string(prev)+"->"+to_string(nums[i-1]));
+                prev = nums[i];
             }
-            i++;
         }
-                if(start==end){
-                    ans.push_back(to_string(start));
-                }
-                else
-                ans.push_back(to_string(start)+"->"+to_string(end));
+        if(prev==nums[nums.size()-1])
+            ans.push_back(to_string(prev));
+        else
+        ans.push_back(to_string(prev)+"->"+to_string(nums[nums.size()-1]));
         return ans;
     }
 };
