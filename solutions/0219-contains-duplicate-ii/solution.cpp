70fe0ccdd4cf1df1c45f5a5b1d4ffe39d6a0ad8c
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-08-19 07:26:04 +0000

    [LeetCode Sync] Runtime - 78 ms (45.38%), Memory - 98.7 MB (45.84%)

--- solutions/0219-contains-duplicate-ii/solution.cpp
+++ solutions/0219-contains-duplicate-ii/solution.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        unordered_set<int>s;
-        int n = nums.size();
-        for(int i=0;i<min(k+1,n);i++){
-            if(s.find(nums[i])!=s.end()){
-                return true;
-            }
-            s.insert(nums[i]);
+        unordered_map<int,int> m;
+        for(int i=0;i<=min(k,(int)nums.size()-1);i++){
+            m[nums[i]]++;
+            if(m[nums[i]]>1) return true;
         }
-        int i=k+1;
-        int j=0;
-        while(i<nums.size()){
-             s.erase(nums[j]);
-             if(s.find(nums[i])!=s.end()){
+        //sliding window
+        int left = 0;
+        for(int right=k+1;right<nums.size();right++){
+            //add right in sliding window
+            m[nums[right]]++;
+            //remove left from sliding window
+            m[nums[left]]--;
+            
+            if(m[nums[right]]>1){
                 return true;
             }
-            s.insert(nums[i]);
-            j++;
-            i++;
+            left++;
         }
         return false;
     }
