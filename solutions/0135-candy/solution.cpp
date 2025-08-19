861af69de884820cd5ad01c05eb2ed54e94dfe11
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-08-19 16:06:07 +0000

    [LeetCode Sync] Runtime - 18 ms (5.18%), Memory - 25.5 MB (5.25%)

--- solutions/0135-candy/solution.cpp
+++ solutions/0135-candy/solution.cpp
@@ -1,26 +1,48 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-        vector<int> values(ratings.size(),1);
-
-        // a>b
-        for(int i=ratings.size()-2;i>=0;i--){
-            if(ratings[i]>ratings[i+1]){
-                values[i]=values[i+1]+1;
-            }
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> min_heap;
+        int i=0;
+        for(auto&r:ratings){
+            min_heap.push({r,i});
+            i++;
         }
-
-        // a<b
-        for(int i=1;i<ratings.size();i++){
-            if(ratings[i-1]<ratings[i] and values[i-1]>=values[i]){
-                values[i]=values[i-1]+1;
+        vector<int> v(ratings.size(),0);
+        int sum = 0;
+        while(!min_heap.empty()){
+            pair<int,int> p = min_heap.top();min_heap.pop();
+            int i= p.second;
+            int leftr = INT_MAX;
+            int rightr = INT_MAX;
+            int leftv = 0;
+            int rightv = 0;
+            int currr = ratings[i];
+            if(i-1>=0){
+                leftr = ratings[i-1];
+                leftv = v[i-1];
             }
-        
+            if(i+1<ratings.size()){
+                rightr = ratings[i+1];
+                rightv = v[i+1];
+            }
+            //higher point
+            if(currr>leftr and currr>rightr){
+                v[i] = max(1+leftv,1+rightv);
+            }
+            else if(currr<leftr and currr<rightr){
+                 v[i]=1;
+            }
+            else if(currr>leftr){
+                 v[i]=leftv+1;
+            }
+            else if(currr>rightr){
+                v[i]=rightv+1;
+            }
+            else {
+                v[i]=1;
+            }
+            sum+= v[i];
         }
-        int sum =0;
-        for(auto i:values)
-        sum+=i;
-
         return sum;
     }
 };
