26847ba41932b28ae2228cfd37e363d019a6475a
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-08-19 03:16:35 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 11.1 MB (11.61%)

--- solutions/0067-add-binary/solution.cpp
+++ solutions/0067-add-binary/solution.cpp
@@ -1,61 +1,57 @@
 class Solution {
 public:
-
-
     string addBinary(string a, string b) {
         string ans = "";
-        char carry='0';
-        int i = a.size()-1;
-        int j = b.size()-1;
-        char first = '0';
-        char second = '0';
-        while(i>=0 or j>=0){
 
-            if(i<0 and j>=0){
-                first = '0';
-                second= b[j]; 
-                j--;
+        char carry = '0';
+        for(int i=0;i<max(a.size(),b.size());i++){
+            char first = '0';
+            char second = '0';
+            if(i<a.size()){
+                first = a[a.size()-i-1];
             }
-            else if(j<0 and i>=0){
-                first = a[i];
-                second = '0';
-                i--;
-            }
-            else if(i>=0 and j>=0){
-                first = a[i];
-                second = b[j];
-                i--;
-                j--;
+            if(i<b.size()){
+                second = b[b.size()-i-1];
             }
 
-            if(((first=='0' and second=='1') or (first=='1' and second=='0')) and carry=='0'){
-                carry = '0';
-                ans='1'+ans;
+            char result = '0';
+
+            if(first=='1' and second=='1'){
+                if(carry=='1'){
+                    result = '1';
+                    carry = '1';
+                }
+                else{
+                    result = '0';
+                    carry = '1';
+                }
             }
-            else if(((first=='0' and second=='1') or (first=='1' and second=='0')) and carry=='1'){
-                carry = '1';
-                ans='0'+ans;
+            else if((first=='0' and second=='1') or (first=='1' and second =='0')){
+                if(carry=='1'){
+                    result = '0';
+                    carry = '1';
+                }
+                else{
+                    result ='1';
+                    carry = '0';
+                }
             }
-            else if(first=='0' and second=='0' and carry =='0'){
-                carry = '0';
-                ans='0'+ans;
+            else{
+                if(carry=='1'){
+                    result = '1';
+                    carry = '0';
+                }
+                else{
+                    result = '0';
+                    carry = '0';
+                }
             }
-            else if(first=='1' and second=='1' and carry =='0'){
-                carry = '1';
-                ans='0'+ans;
-            }
-            else if(first=='0' and second=='0' and carry =='1'){
-                carry = '0';
-                ans='1'+ans;
-            }
-            else {
-                carry = '1';
-                ans='1'+ans;
-            }
+
+            ans = result+ans;
+
         }
-
         if(carry=='1'){
-            ans='1'+ans;
+            ans = carry + ans;
         }
         return ans;
     }
