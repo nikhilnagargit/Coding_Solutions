b4aab74715d385a7022d9c26edce8e67a4960de1
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-08-19 13:46:24 +0000

    [LeetCode Sync] Runtime - 7 ms (24.21%), Memory - 11.7 MB (88.89%)

--- solutions/0383-ransom-note/solution.cpp
+++ solutions/0383-ransom-note/solution.cpp
@@ -1,27 +1,14 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        map<char,int>m;
-        for( int i=0;i<magazine.size();i++){
-            if(m.find(magazine[i])==m.end()){
-                m[magazine[i]] = 1;
-            }
-            else{
-                m[magazine[i]]+=1;
-            }
+        unordered_map<char,int> m;
+        for(auto& c:magazine){
+            m[c]++;
         }
-
-
-        for(int i=0;i<ransomNote.size();i++){
-            if(m.find(ransomNote[i])==m.end()){
-                return false;
-            }
-            else if(m[ransomNote[i]]==0){
-                return false;
-            }
-            else{
-                m[ransomNote[i]] -=1;
-            }
+        for(auto&c: ransomNote){
+            if(m.find(c)==m.end()) return false;
+            if(m[c]==0) return false;
+            m[c]--;
         }
         return true;
     }
