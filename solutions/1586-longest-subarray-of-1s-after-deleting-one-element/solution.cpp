6de8440088dd4419c6dfdbfc9a8777819922e405
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-08-24 06:31:48 +0000

    [LeetCode Sync] Runtime - 2 ms (29.99%), Memory - 60.1 MB (49.02%)

--- solutions/1586-longest-subarray-of-1s-after-deleting-one-element/solution.cpp
+++ solutions/1586-longest-subarray-of-1s-after-deleting-one-element/solution.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
-        int left  =0 ;
-        int right =0;
-        int maxi = 0;
-        bool isdeleted = false;
-        while(right<nums.size()){
-            if(nums[right]==1){
-                right++;
+        //sliding window
+        int left = 0;
+        int maxlength = 0;
+        int zeros = 0;
+        for(int right=0;right<nums.size();right++){
+            if(nums[right]!=1){
+                zeros+=1;
             }
-            else if(nums[right]==0 and isdeleted==false){
-                right++;
-                isdeleted = true;
-            }
-            else {
+            if(zeros>1){
                 while(nums[left]!=0){
                     left++;
                 }
                 left++;
-                right++;
+                zeros -= 1;
             }
-            maxi = max(maxi,right-left-1);
+            maxlength = max(maxlength,right-left);
         }
-        return maxi;
+        return maxlength;
     }
 };
