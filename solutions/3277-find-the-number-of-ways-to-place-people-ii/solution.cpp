9c50f98b67d4e93d8ec529fe862c9b3ff45a1e88
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-09-03 15:23:44 +0000

    [LeetCode Sync] Runtime - 124 ms (58.21%), Memory - 41.2 MB (91.04%)

--- solutions/3277-find-the-number-of-ways-to-place-people-ii/solution.cpp
+++ solutions/3277-find-the-number-of-ways-to-place-people-ii/solution.cpp
@@ -1,54 +1,32 @@
+class cmp{
+    public:
+    bool operator()(vector<int>& a,vector<int>& b){
+        if(a[0]<b[0]){
+            return true;
+        }
+        else if(a[0]==b[0]){
+            return a[1]>=b[1];
+        }
+        return false;
+    }
+};
+
 class Solution {
 public:
-bool isvalid(vector<vector<int>>&points, int I,int J){
-        int x1= points[I][0];
-        int y1= points[I][1];
-        int x2= points[J][0];
-        int y2= points[J][1];
-        
-        if(x1==x2 or y1==y2){
-            
-        }
-        else if(x1<x2){
-            if(y1>=y2){
-            
-            }
-            else{
-                return false;
-            }
-        }
-        else{
-            if(y1>y2){
-                return false;
-            }
-        }
-                       
-        for(int i=0;i<points.size();i++){
-            if(i!=I and i!=J){
-                int a = points[i][0];
-                int b = points[i][1];
-                int X  = abs(x2-x1);
-                int Y  = abs(y2-y1);
-                int dis_ax1 = abs(x1-a);
-                int dis_ax2 = abs(x2-a);
-                int dis_by1 = abs(y1-b);
-                int dis_by2 = abs(y2-b);
-                if(dis_ax1<=X and dis_ax2<=X and dis_by1<=Y and dis_by2<=Y){
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
     int numberOfPairs(vector<vector<int>>& points) {
-        int ans = 0;
+        int count =0;
+        sort(points.begin(),points.end(),cmp());
         for(int i=0;i<points.size();i++){
+            int ymax = points[i][1];
+            int ymin = INT_MIN;
             for(int j=i+1;j<points.size();j++){
-                
-                if(isvalid(points,i,j))
-                    ans++;
+                if(points[j][1]>ymax or points[j][1]<=ymin){
+                    continue;
+                }
+                count++;
+                ymin = points[j][1];
             }
         }
-        return ans;
+        return count;
     }
 };
