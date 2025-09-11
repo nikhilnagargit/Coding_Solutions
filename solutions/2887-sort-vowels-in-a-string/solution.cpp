85ea62932a2983f492cb85165ecdac9a3931a62c
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-09-11 04:19:41 +0000

    [LeetCode Sync] Runtime - 25 ms (16.46%), Memory - 16.8 MB (24.64%)

--- solutions/2887-sort-vowels-in-a-string/solution.cpp
+++ solutions/2887-sort-vowels-in-a-string/solution.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
     string sortVowels(string s) {
-        vector<int> vowel_indexes;
-        vector<int> vowels;
-        set<char>V = {'a','A','e','E','i','I','O','o','u','U'}; 
+        unordered_map<char,int> freq;
+        unordered_set<char>vowels = {'a','e','i','o','u','A','E','I','O','U'};
         for(int i=0;i<s.size();i++){
-            if(V.find(s[i])!=V.end()){
-                vowel_indexes.push_back(i);
-                vowels.push_back(s[i]);
+            if(vowels.count(s[i])){
+                freq[s[i]]++;
             }
         }
-        sort(vowels.begin(),vowels.end());
-        for(int i=0;i<vowel_indexes.size();i++){
-            s[vowel_indexes[i]] = vowels[i];
+
+        vector<int> items ;
+        for(auto& item:freq){
+            items.push_back(item.first);
+        }
+        sort(items.begin(),items.end());
+        int p = 0 ;
+        for(int i = 0;i<s.size();i++){
+            if(vowels.count(s[i])){
+                s[i]=items[p];
+                freq[items[p]]--;
+                if(freq[items[p]]==0){
+                    p++;
+                }
+            }
         }
         return s;
     }
