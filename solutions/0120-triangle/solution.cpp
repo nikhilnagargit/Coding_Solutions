5289ea143372bb921b0a89a7fe4883aafc18e780
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-09-25 04:39:33 +0000

    [LeetCode Sync] Runtime - 3 ms (13.23%), Memory - 13.9 MB (7.75%)

--- solutions/0120-triangle/solution.cpp
+++ solutions/0120-triangle/solution.cpp
@@ -3,19 +3,32 @@ public:
     int minimumTotal(vector<vector<int>>& triangle) {
         vector<int> dp;
         dp.push_back(triangle[0][0]);
-        for(int i=1;i<triangle.size();i++){
-            vector<int> dp_copy = dp;
-            dp_copy.push_back(0);
-            for(int j=0;j<triangle[i].size();j++){
-                if(j-1>=0 and j<dp.size())
-                dp_copy[j]=min(triangle[i][j]+dp[j-1],triangle[i][j]+dp[j]);
-                else if(j-1>=0)
-                dp_copy[j]=triangle[i][j]+dp[j-1];
-                else
-                dp_copy[j]=triangle[i][j]+dp[j];
+        for(int row=1;row<triangle.size();row++){
+            vector<int> temp = dp;
+            temp.push_back(0);
+            for(int i=0;i<triangle[row].size();i++){
+                //find two parent's indexes from where we can come here
+                int idx1 = i;
+                int idx2 = i-1;
+                
+                //put the value in new dp row
+                int value1 = INT_MAX/2;
+                int value2 = INT_MAX/2;
+
+                if(idx1<dp.size()){
+                    value1 = dp[idx1];
+                }
+                if(idx2>=0){
+                    value2 = dp[idx2];
+                }
+                temp[i] = triangle[row][i] + min({value1,value2});
             }
-            dp = dp_copy;
+            dp = temp;
         }
-        return *min_element(dp.begin(),dp.end());
+        int mini = INT_MAX;
+        for(auto& i:dp){
+            mini = min(mini,i);
+        }
+        return mini;
     }
 };
