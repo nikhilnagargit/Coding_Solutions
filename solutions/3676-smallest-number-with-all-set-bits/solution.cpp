1565315848d10b531cebadcdc53aee09317fb0d8
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-10-29 05:24:21 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.2 MB (87.69%)

--- solutions/3676-smallest-number-with-all-set-bits/solution.cpp
+++ solutions/3676-smallest-number-with-all-set-bits/solution.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
     int smallestNumber(int n) {
-        int ans = 1;
-        for(int i=0;i<n;i++){
-            ans = ans*2;
-            if(ans-1>=n){
-                break;
+        int ans = 0;
+        int flag = 0;
+        for(int i=31;i>=0;i--){
+            if(flag){
+                ans = ans|(1<<i);
+            }
+            else{
+                if(((n)&(1<<i))){
+                    flag = 1;
+                    ans = ans|(1<<i);
+                }
             }
         }
-        return ans-1;
-        
+        return ans;
     }
 };
