31e6f9d624f83f288a275fbe2de37a245b5db07a
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-11-02 03:41:44 +0000

    [LeetCode Sync] Runtime - 56 ms (69.34%), Memory - 173.1 MB (55.08%)

--- solutions/2343-count-unguarded-cells-in-the-grid/solution.cpp
+++ solutions/2343-count-unguarded-cells-in-the-grid/solution.cpp
@@ -1,77 +1,114 @@
 class Solution {
 public:
+
+    void printboard(vector<vector<int>>& grid){
+        int m = grid.size();
+        int n = grid[0].size();
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                cout<<grid[i][j]<<", ";
+            }
+            cout<<endl;
+        }
+
+        cout<<"-----------------"<<endl;
+    }
     int countUnguarded(int m, int n, vector<vector<int>>& guards, vector<vector<int>>& walls) {
-        vector<vector<int>> arr(m,vector<int>(n,0));
-
-        //2 = wall
-        //3 = guard
-        int total = m*n - walls.size() - guards.size();
-
-        for(auto& i:walls){
-            arr[i[0]][i[1]] = 2;
+        vector<vector<int>> grid(m,vector<int>(n,0));
+        // 0 = unguarded  , 1 = guard,  2 = wall ,  3 = guarded
+        for(auto& g: guards){
+            grid[g[0]][g[1]] = 1;
         }
-        for(auto& i:guards){
-            arr[i[0]][i[1]] = 3;
+        for(auto& w:walls){
+            grid[w[0]][w[1]] = 2;
         }
 
-        for(auto& g:guards){
-
-            int i = g[0];
-            int j = g[1];
-            //up
-            for(int k=i-1;k>=0;k--){
-                if(arr[k][j]==2 or arr[k][j]==3){
-                    break;
+        //passes
+        bool guardingActive = 0;
+        //left to right
+        for(int i=0;i<m;i++){
+            guardingActive = 0;
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1){
+                    guardingActive = 1;
                 }
-                else if(arr[k][j]==0){
-                    arr[k][j] = 1;
-                    total--;
+                else if(grid[i][j]==2){
+                    guardingActive = 0;
                 }
-            }
-
-            //left
-            for(int k=j-1;k>=0;k--){
-                if(arr[i][k]==2 or arr[i][k]==3){
-                    break;
-                }
-                else if(arr[i][k]==0){
-                    arr[i][k] = 1;
-                    total--;
+                else{
+                    if(guardingActive)
+                    grid[i][j] = 3;
                 }
             }
-
-            //right
-            for(int k=j+1;k<n;k++){
-                if(arr[i][k]==2 or arr[i][k]==3){
-                    break;
-                }
-                else if(arr[i][k]==0){
-                    arr[i][k] = 1;
-                    total--;
-                }
-            }
-            //down
-            for(int k=i+1;k<m;k++){
-                if(arr[k][j]==2 or arr[k][j]==3){
-                    break;
-                }
-                else if(arr[k][j]==0){
-                    arr[k][j] = 1;
-                    total--;
-                }
-            }
-
         }
 
+        // printboard(grid);
+        //right to left
+      
+        for(int i=0;i<m;i++){
+            guardingActive = 0;
+            for(int j=n-1;j>=0;j--){
+                if(grid[i][j]==1){
+                    guardingActive = 1;
+                }
+                else if(grid[i][j]==2){
+                    guardingActive = 0;
+                }
+                else{
+                    if(guardingActive)
+                    grid[i][j] = 3;
+                }
+            }
+        }
+//    printboard(grid);
+        //top to down
 
-        // int count = 0;
-        // for(int i=0;i<m;i++){
-        //     for(int j=0;j<n;j++){
-        //         if(arr[i][j]==0){
-        //             count++;
-        //         }
-        //     }
-        // }
-        return total;
+        for(int j=0;j<n;j++){
+            guardingActive = 0;
+          for(int i=0;i<m;i++){
+                if(grid[i][j]==1){
+                    guardingActive = 1;
+                }
+                else if(grid[i][j]==2){
+                    guardingActive = 0;
+                }
+                else{
+                    if(guardingActive)
+                    grid[i][j] = 3;
+                }
+            }
+        }
+
+//    printboard(grid);
+        //down to top
+      
+        for(int j=0;j<n;j++){
+            guardingActive = 0;
+            for(int i=m-1;i>=0;i--){
+                if(grid[i][j]==1){
+                    guardingActive = 1;
+                }
+                else if(grid[i][j]==2){
+                    guardingActive = 0;
+                }
+                else{
+                    if(guardingActive)
+                    grid[i][j] = 3;
+                }
+            }
+        }
+//    printboard(grid);
+        int count = 0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==0){
+                    count++;
+                }
+            }
+        }
+
+        return count;
+
     }
 };
