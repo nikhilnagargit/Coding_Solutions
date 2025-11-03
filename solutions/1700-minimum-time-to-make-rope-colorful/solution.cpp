3682e78e4bd7eaef578dcad6ede15fe3ee734bab
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-11-03 03:02:32 +0000

    [LeetCode Sync] Runtime - 10 ms (18.18%), Memory - 99.9 MB (33.59%)

--- solutions/1700-minimum-time-to-make-rope-colorful/solution.cpp
+++ solutions/1700-minimum-time-to-make-rope-colorful/solution.cpp
@@ -1,31 +1,23 @@
 class Solution {
 public:
     int minCost(string colors, vector<int>& neededTime) {
-        int n = colors.size();
-        vector<int> dp(n, -1);
-        return calculateMinCost(n - 1, colors, neededTime, dp, 'A', neededTime[n - 1]);
-    }
-
-    // Recursive function to calculate the minimum cost
-    int calculateMinCost(int i, string& colorSequence, vector<int>& timeRequired, vector<int>& memo, char previousColor, int previousTime) {
-        if (i < 0) {
-            // Base case: Reached the beginning of the sequence
-            return 0;
+        int sum = neededTime[0];
+        int maxi = neededTime[0];
+        int cost = 0 ;
+        for(int i=1;i<colors.size();i++){
+            if(colors[i-1]!=colors[i]){
+                //caclulate cost
+                cost += sum-maxi;
+                //reset
+                sum = neededTime[i];
+                maxi = neededTime[i];
+            }
+            else{
+                sum+= neededTime[i];
+                maxi = max(maxi,neededTime[i]);
+            }
         }
-
-        if (memo[i] != -1) {
-            // If the sub-problem is solved earlier, return the stored result
-            return memo[i];
-        }
-
-        if (colorSequence[i] == previousColor) {
-            // If the current color is the same as the previous one, choose the minimum time
-            return memo[i] = calculateMinCost(i - 1, colorSequence, timeRequired, memo, colorSequence[i], max(timeRequired[i], previousTime)) + min(timeRequired[i], previousTime);
-        } else {
-            // If the current color is different, choose the time required for the current color
-            return memo[i] = calculateMinCost(i - 1, colorSequence, timeRequired, memo, colorSequence[i], timeRequired[i]);
-        }
+        cost += sum-maxi;
+        return cost;
     }
 };
-
-
