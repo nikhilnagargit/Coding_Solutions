a94c2d4f0195c3218066cc64eff731f3ffd3df3d
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-11-16 08:20:15 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18.6 MB (87.58%)

--- solutions/0073-set-matrix-zeroes/solution.cpp
+++ solutions/0073-set-matrix-zeroes/solution.cpp
@@ -1,73 +1,55 @@
 class Solution {
-
 public:
-
     void setZeroes(vector<vector<int>>& matrix) {
-        // rowwise
-        int isFirstRowZero = 0;
-        int isFirstColZero = 0;
-
-        for(int i=0;i<matrix.size();i++){
+        bool isFirstRowZero = false;
+        bool isFirstColZero = false;
+        int m = matrix.size();
+        int n = matrix[0].size();
+        //m = rows , n = cols
+        for(int i=0;i<m;i++){
             if(matrix[i][0]==0){
-                isFirstColZero = 1;
-                break;
+                isFirstColZero = true;
             }
         }
-
-
-        for(int j=0;j<matrix[0].size();j++){
+        for(int j=0;j<n;j++){
             if(matrix[0][j]==0){
-                isFirstRowZero = 1;
-                break;
+                isFirstRowZero = true;
             }
         }
-
-
-
-        for(int i=1;i<matrix.size();i++){
-            for(int j=1;j<matrix[0].size();j++){
+        //traverse submatrix with one less row and one less col
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
                 if(matrix[i][j]==0){
-                    matrix[0][j] = 0;
-                    matrix[i][0] = 0;
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
                 }
             }
         }
-    
-
-
-        for(int i=1;i<matrix.size();i++){
-            if(matrix[i][0]==0){
-                // make row zero
-                for(int j=0;j<matrix[0].size();j++){
-                    matrix[i][j]=0;
-                }
+        //normalize all zeros row wise
+        for(int i=1;i<m;i++){
+            if(matrix[i][0]==0)
+            for(int j=1;j<n;j++){
+                matrix[i][j]=0;
             }
         }
-
-
-        for(int j=1;j<matrix[0].size();j++){
-            if(matrix[0][j]==0){
-                // make col zero
-                for(int i=0;i<matrix.size();i++){
-                    matrix[i][j]=0;
-                }
+        //normalize all zeros col wise
+        for(int j=1;j<n;j++){
+            if(matrix[0][j]==0)
+            for(int i=1;i<m;i++){
+                matrix[i][j]=0;
             }
         }
-        
-
-
-        if(isFirstColZero){
-            for(int i=0;i<matrix.size();i++){
-                matrix[i][0] = 0;
-            }
-        }
-
+        //normalize first row and first col
         if(isFirstRowZero){
-            for(int j=0;j<matrix[0].size();j++){
+            for(int j=0;j<n;j++){
                 matrix[0][j]=0;
             }
         }
-  
-        
+        if(isFirstColZero){
+            for(int i=0;i<m;i++){
+                matrix[i][0]=0;
+            }
+        }
+
     }
 };
