3e9ed4b1b197a744b5675f7af4aa5e39206c2478
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-11-17 05:00:19 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 12.2 MB (96.38%)

--- solutions/0088-merge-sorted-array/solution.cpp
+++ solutions/0088-merge-sorted-array/solution.cpp
@@ -1,38 +1,25 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector<int> v(m+n,0);
-
-        int i=0;
-        int j=0;
-        int k=0;
-
-        while(i<m and j<n){
-            if(nums1[i]<=nums2[j]){
-                v[k]=nums1[i];
-                k++;
-                i++;
+        int p = m+n-1;
+        int end1 = m-1;
+        int end2 = n-1;
+        while(end2>=0 and end1>=0){
+            if(nums1[end1]<=nums2[end2]){
+                nums1[p]=nums2[end2];
+                end2--;
             }
             else{
-                v[k]=nums2[j];
-                k++;
-                j++;
+                nums1[p]=nums1[end1];
+                end1--;
             }
+            p--;
         }
-
-        while(i<m){
-            v[k] = nums1[i];
-            k++;
-            i++;
+        while(end2>=0){
+            nums1[p]=nums2[end2];
+            end2--;
+            p--;
         }
-
-        while(j<n){
-            v[k] = nums2[j];
-            k++;
-            j++;
-        }
-
-        nums1 = v;
 
     }
 };
