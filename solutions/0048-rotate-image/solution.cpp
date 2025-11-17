0bbaf2807d464fb7bdb155a5e4a93c38032fe5f2
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-11-17 04:28:39 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 10.3 MB (35.90%)

--- solutions/0048-rotate-image/solution.cpp
+++ solutions/0048-rotate-image/solution.cpp
@@ -1,27 +1,18 @@
 class Solution {
 public:
-
-
     void rotate(vector<vector<int>>& matrix) {
-        int m = matrix[0].size();
+        // swap over diagonal
         int n = matrix.size();
-
-        // transpose 
-        for (int i=0;i<n;i++){
-            for(int j=i;j<m;j++){
-                swap(matrix[i][j],matrix[j][i]);
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-1-i;j++){
+                swap(matrix[i][j],matrix[n-j-1][n-i-1]);
             }
         }
-        // printM(matrix);
-
-        // flip
-        for (int j=0;j<=(m-1)/2;j++){
-            for(int i=0;i<n;i++){
-                swap(matrix[i][j],matrix[i][m-j-1]);
+        // flip upside down
+        for(int i=0;i<n/2;i++){
+            for(int j=0;j<n;j++){
+                swap(matrix[i][j],matrix[n-1-i][j]);
             }
         }
-
-        // printM(matrix);
-
     }
 };
