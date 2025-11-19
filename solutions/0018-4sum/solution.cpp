505fbe0b071ab08b0a0ee7bfd0f9586d74d3b856
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-11-19 17:25:22 +0000

    [LeetCode Sync] Runtime - 23 ms (55.42%), Memory - 17.4 MB (53.39%)

--- solutions/0018-4sum/solution.cpp
+++ solutions/0018-4sum/solution.cpp
@@ -2,29 +2,27 @@ class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         vector<vector<int>> ans;
+        if(nums.size()<4){
+            return {};
+        }
         sort(nums.begin(),nums.end());
-        if(nums.size()<=3) return ans;
         for(int i=0;i<nums.size()-3;i++){
-            if(i>0 and nums[i]==nums[i-1]) continue;
+            if(i>0 and nums[i-1]==nums[i]) continue;
             for(int j=i+1;j<nums.size()-2;j++){
-                if(j>i+1 and nums[j]==nums[j-1]){continue;}
-                long long rem_sum = 1ll*target-(1ll*nums[i]+1ll*nums[j]);
+                if(j>i+1 and nums[j-1]==nums[j]) continue;
                 int left = j+1;
                 int right = nums.size()-1;
                 while(left<right){
-                    int sum = nums[left]+nums[right];
-                    if(sum==rem_sum){
+                    if((1ll*nums[left]+1ll*nums[right]+1ll*nums[i]+1ll*nums[j])==target){
                         ans.push_back({nums[i],nums[j],nums[left],nums[right]});
-                        while(left+1<right and nums[left]==nums[left+1]){
-                            left++;
-                        }
-                        while(left+1<right and nums[right-1]==nums[right]){
-                            right--;
-                        }
+                        while(left<right and nums[left]==nums[left+1])
+                        left++;
+                        while(left<right and nums[right]==nums[right-1])
+                        right--;
                         left++;
                         right--;
                     }
-                    else if(sum>rem_sum){
+                    else if(1ll*nums[left]+1ll*nums[right]+1ll*nums[i]+1ll*nums[j]>target){
                         right--;
                     }
                     else{
