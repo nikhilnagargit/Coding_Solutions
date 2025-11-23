3b34a30f1657679cc73c4f72ae9567eb2e348980
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-11-23 08:28:42 +0000

    [LeetCode Sync] Runtime - 3 ms (91.67%), Memory - 15.3 MB (27.34%)

--- solutions/0138-copy-list-with-random-pointer/solution.cpp
+++ solutions/0138-copy-list-with-random-pointer/solution.cpp
@@ -17,50 +17,25 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        Node* head2_parent = new Node(0);
-        Node* h2 = head2_parent;
-        Node* h1 = head;
-        // map address to idx and store
-        unordered_map<Node*,int> old_add_idx;
-        int i=1;
-        while(h1){
-            old_add_idx[h1] = i;
-            h1 = h1->next;
-            i++;
+        unordered_map<Node*,Node*> oldNewMap;
+        Node* newhead = new Node(0);
+        Node* currnew = newhead;
+        Node* curr = head;
+        while(curr){
+            Node* temp = new Node(curr->val);
+            currnew->next = temp;
+            currnew = temp;
+            oldNewMap[curr] = temp;
+            curr = curr->next;
         }
-        //reset h1
-        h1 = head;
-
-
-        //create new ll and map index of random also
-        unordered_map<int,int> mp_idx_idx;
-        unordered_map<int,Node*> new_idx_add;
-        i=1;
-        while(h1){
-            h2->next = new Node(h1->val);
-            new_idx_add[i] = h2->next;
-            mp_idx_idx[i] = old_add_idx[h1->random];
-            h1 = h1->next;
-            h2 = h2->next;
-            i++;
+        newhead =newhead->next;
+        currnew = newhead;
+        Node* currold = head;
+        while(currnew){
+            currnew->random = oldNewMap[currold->random];
+            currnew = currnew->next;
+            currold = currold->next;
         }
-        
-        //reset h1 and h2
-        h2 = head2_parent;
-        h1 = head;
-
-
-
-        // map address to idx for new ll
-        h2= h2->next;
-        i=1;
-        while(h2){
-            h2->random = new_idx_add[mp_idx_idx[i]];
-            i++;
-            h2 = h2->next;
-        }
-
-
-        return head2_parent->next;
+        return newhead;
     }
 };
