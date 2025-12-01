d83d83d385e09380bf439f715b145ba348c6369f
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-12-01 06:01:33 +0000

    [LeetCode Sync] Runtime - 7 ms (6.67%), Memory - 15.3 MB (5.03%)

--- solutions/0046-permutations/solution.cpp
+++ solutions/0046-permutations/solution.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
-    vector<vector<int>>ans;
-     
-    void solve(unordered_set<int>& s, vector<int>& v){
+    void solve(vector<int>& v, vector<vector<int>>& ans,unordered_set<int> s){
         if(s.size()==0){
             ans.push_back(v);
             return;
         }
-        unordered_set<int> s_copy = s;
+
         for(auto item:s){
             v.push_back(item);
+            unordered_set<int>s_copy = s;
             s_copy.erase(item);
-            solve(s_copy,v);
-            s_copy.insert(item);
+            solve(v,ans,s_copy);
             v.pop_back();
         }
-
     }
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<int>v;
+        vector<vector<int>> ans;
+        vector<int> v;
         unordered_set<int> s(nums.begin(),nums.end());
-        solve(s,v);
+        solve(v,ans,s);
         return ans;
     }
 };
