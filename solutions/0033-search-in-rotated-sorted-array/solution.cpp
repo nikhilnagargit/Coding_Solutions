f4785cb710019a9afd8c2b3c914aad46e416c5aa
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2025-12-07 17:17:05 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 15.1 MB (66.76%)

--- solutions/0033-search-in-rotated-sorted-array/solution.cpp
+++ solutions/0033-search-in-rotated-sorted-array/solution.cpp
@@ -1,43 +1,33 @@
 class Solution {
 public:
-    int binarySearch(vector<int>& nums,int target,int left,int right){
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right= nums.size()-1;
+        int mid = 0;
         while(left<=right){
-            int mid = left +(right-left)/2;
+            mid = left + (right-left)/2;
             if(nums[mid]==target){
                 return mid;
             }
-            else if(nums[mid]>target){
-                right = mid-1;
+            else if(nums[left]>nums[mid]){
+                if(target>=nums[mid] and target<=nums[right]){
+                left =  mid+1;
+                }
+                else{
+                 
+                           right = mid-1;
+                }
             }
             else{
-                left = mid+1;
+                if(nums[left]<=target and target<=nums[mid]){
+                    right = mid-1;
+                }
+                else{
+                    left = mid+1;
+                }
             }
+  
         }
-        return -1;
-    }
-
-    int search(vector<int>& nums, int target) {
-        int left = 0;
-        int right = nums.size()-1;
-        // first find the peak
-        while(left<right){
-            int mid = left + (right-left)/2;
-            // left side sorted
-            if(nums[mid]>nums[right]){
-                left = mid+1;
-            }
-            //right side sorted
-            else{
-                right = mid;
-            }
-        }
-        int peak = left;
-        cout<<peak;
-        int first = binarySearch(nums,target,0,peak-1);
-        if(first!=-1) return first;
-
-        int second = binarySearch(nums,target,peak,nums.size()-1);
-        if(second!=-1) return second;
 
         return -1;
     }
