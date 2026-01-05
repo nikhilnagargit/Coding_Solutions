c06d56b5fcb47f15a2193a90ffe332699ed5c71d
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2026-01-05 06:12:35 +0000

    [LeetCode Sync] Runtime - 222 ms (23.69%), Memory - 39.5 MB (99.97%)

---
+++ solutions/0295-find-median-from-data-stream/solution.py
@@ -0,0 +1,29 @@
+import heapq
+
+class MedianFinder:
+    def __init__(self):
+        self.minHeap = []
+        self.maxHeap = []
+        
+    def addNum(self, num: int) -> None:
+
+        heapq.heappush(self.maxHeap,-num)
+        heapq.heappush(self.minHeap,-heapq.heappop(self.maxHeap))
+ 
+        # balance the heaps
+        if len(self.maxHeap)<len(self.minHeap):
+            heapq.heappush(self.maxHeap,-heapq.heappop(self.minHeap))
+
+
+
+    def findMedian(self) -> float:
+        if len(self.maxHeap)>len(self.minHeap):
+            return -self.maxHeap[0]
+        else:
+            return (-self.maxHeap[0]+self.minHeap[0])/2.0
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
