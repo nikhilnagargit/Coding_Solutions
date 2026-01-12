29b150a4a44ff56db9f483600b4dfa24c1f9783e
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2026-01-12 07:59:42 +0000

    [LeetCode Sync] Runtime - 52 ms (80.00%), Memory - 24.2 MB (22.59%)

---
+++ solutions/0937-online-stock-span/solution.py
@@ -0,0 +1,11 @@
+# Python
+class StockSpanner:
+    def __init__(self):
+        self.stack = []  # Each element: [price, span]
+
+    def next(self, price: int) -> int:
+        span = 1
+        while self.stack and self.stack[-1][0] <= price:
+            span += self.stack.pop()[1]
+        self.stack.append([price, span])
+        return span
