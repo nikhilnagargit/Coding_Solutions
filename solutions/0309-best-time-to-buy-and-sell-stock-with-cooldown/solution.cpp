28fe505801e0d2b96c37c4353d354ea72917d24a
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2026-01-24 09:01:10 +0000

    [LeetCode Sync] Runtime - 35 ms (5.01%), Memory - 20.9 MB (5.34%)

--- solutions/0309-best-time-to-buy-and-sell-stock-with-cooldown/solution.cpp
+++ solutions/0309-best-time-to-buy-and-sell-stock-with-cooldown/solution.cpp
@@ -1,36 +1,39 @@
 class Solution {
 public:
-    int solve(vector<int>& prices,int i,int action,vector<vector<int>>& dp){
+    int solve(vector<int>& prices,int i,int action,unordered_map<string,int>& m){
         if(i==prices.size()){
             return 0;
         }
-        if(dp[i][action]!=-1) return dp[i][action];
-        int maxi = 0;
-        //if action is to buy
+        string key = to_string(i) + '-' + to_string(action);
+        if(m.find(key)!=m.end()){
+            return m[key];
+        }
+
+        // cooldown
+        if(action==0){
+            // set next action to buy and skip current stock
+            return m[key]=solve(prices,i+1,-1,m);
+        }
+
+        // buy
+        if(action==-1){
+            //skip buy or buy right now
+            return m[key]= max(solve(prices,i+1,-1,m),-prices[i]+solve(prices,i+1,1,m));
+        }
+
+        //sell
         if(action==1){
-            //next action will be sell
-             maxi =  max(maxi,-1*prices[i]+solve(prices,i+1,2,dp));
+            //skip sell or sell right now
+            return m[key]= max(solve(prices,i+1,1,m),prices[i]+solve(prices,i+1,0,m));
         }
-        //aciton is sell
-        else if(action==2){
-            // next action will be cooldown at least
-               maxi = max(maxi, prices[i]+solve(prices,i+1,0,dp));
-        }
-        else if(action==0){
-                maxi = max(maxi,solve(prices,i+1,1,dp));
-        }
-        //you can skip the day and take action later
-        maxi = max(maxi,solve(prices,i+1,action,dp));
-
-        return dp[i][action]=maxi;
+        return 0;
     }
-    int maxProfit(vector<int>& prices) {
-        //actions 
-        // 0 = cooldown
-        // 1 = buy
-        // 2 = sell
-        vector<vector<int>> dp(prices.size(),vector<int>(3,-1));
 
-        return solve(prices,0,1,dp);
+    int maxProfit(vector<int>& prices) {
+    // buy = -1
+    // sell = 1
+    // cooldown =0
+    unordered_map<string,int> m;
+    return solve(prices, 0, -1, m);
     }
 };
