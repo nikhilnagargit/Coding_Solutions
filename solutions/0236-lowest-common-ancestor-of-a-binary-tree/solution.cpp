0de50b8fa41a15f332ac31cd67c36276f7349a92
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2026-01-25 18:15:22 +0000

    [LeetCode Sync] Runtime - 16 ms (27.59%), Memory - 17.2 MB (87.80%)

--- solutions/0236-lowest-common-ancestor-of-a-binary-tree/solution.cpp
+++ solutions/0236-lowest-common-ancestor-of-a-binary-tree/solution.cpp
@@ -9,12 +9,18 @@
  */
 class Solution {
 public:
+
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root==p or root==q or !root) return root;
-        TreeNode* left= lowestCommonAncestor(root->left,p,q);
-        TreeNode* right= lowestCommonAncestor(root->right,p,q);
-        if(!left) return right;
-        if(!right) return left;
-        return root;
+        if(!root or root==p or root==q){
+            return root;
+        }
+        TreeNode* rightAns = lowestCommonAncestor(root->right,p,q);
+        TreeNode* leftAns = lowestCommonAncestor(root->left,p,q);
+        if(rightAns and leftAns){
+            return root;
+        }
+        if(rightAns) return rightAns;
+        if(leftAns) return leftAns;
+        return nullptr;
     }
 };
