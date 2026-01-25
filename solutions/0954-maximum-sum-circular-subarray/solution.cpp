5d38ac16bfd3c48875c5edfde9d5a28ac94e0cf7
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2026-01-25 06:17:11 +0000

    [LeetCode Sync] Runtime - 4 ms (11.78%), Memory - 43.5 MB (96.41%)

--- solutions/0954-maximum-sum-circular-subarray/solution.cpp
+++ solutions/0954-maximum-sum-circular-subarray/solution.cpp
@@ -1,31 +1,36 @@
 class Solution {
 public:
     int maxSubarraySumCircular(vector<int>& nums) {
-        int max_sum = INT_MIN;
-        int sum=0;
+
+        // use kadanes
+        int maxSum = nums[0];
+        int currSum = 0;
+        int total =0 ;
+
         for(int i=0;i<nums.size();i++){
-            if(sum<0){
-                sum=0;
+            currSum+= nums[i];
+            maxSum = max(maxSum,currSum);
+            if(currSum<0){
+                currSum=0;
             }
-            sum+= nums[i];
-            max_sum = max(max_sum,sum);
+            total+= nums[i];
         }
-        int max_sum1 = max_sum;
-        max_sum=INT_MIN;
-        sum=0;
-        int total=0;
+
+
+        // second kadane
+        currSum =0 ;
+        int maxNegSum = -nums[0];
         for(int i=0;i<nums.size();i++){
-            if(sum<0){
-                sum=0;
+            currSum+= -nums[i];
+            maxNegSum = max(maxNegSum,currSum);
+            if(currSum<0){
+                currSum=0;
             }
-            sum+= -nums[i];
-            total+= -nums[i];
-            max_sum = max(max_sum,sum);
-        }
-        int max_sum2 = max_sum;
-        if(max_sum2==total){
-            return max_sum1;
-        }
-        return max(max_sum1,max_sum2-total);
+        }   
+        if(maxNegSum+total==0) return maxSum;
+        return max(maxSum, total+maxNegSum);
+
+        // return maxSum;
+
     }
 };
