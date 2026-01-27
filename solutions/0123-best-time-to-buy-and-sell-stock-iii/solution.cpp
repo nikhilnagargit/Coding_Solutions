07d738a8156bb7f1d4265c67024fac083f375ac5
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2026-01-27 10:17:24 +0000

    [LeetCode Sync] Runtime - 150 ms (49.87%), Memory - 139.7 MB (47.63%)

--- solutions/0123-best-time-to-buy-and-sell-stock-iii/solution.cpp
+++ solutions/0123-best-time-to-buy-and-sell-stock-iii/solution.cpp
@@ -1,26 +1,16 @@
 class Solution {
 public:
-    int solve(vector<int>& prices, int pi, vector<int>& transactions, int ti,vector<vector<int>> & dp){
-        // all transactions completed
-        if(ti==transactions.size() or pi==prices.size()){
-            return 0;
-        }
-        if(dp[pi][ti]!=-1){
-            return dp[pi][ti];
-        }
-    
-        // make the transaction
-        int a= prices[pi]*transactions[ti] + solve(prices,pi+1,transactions, ti+1,dp);
-        // skip the transaction
-        int b = solve(prices,pi+1,transactions,ti,dp);
-
-        return dp[pi][ti]= max(a,b);
-    }
 
     int maxProfit(vector<int>& prices) {
-        // -1 = buy , +1 = sell
-        vector<int> transactions = {-1,1,-1,1};
-        vector<vector<int>> dp(prices.size(), vector<int>(transactions.size(),-1));
-        return solve(prices,0,transactions,0,dp);
+        vector<int> actions = {-1,1,-1,1};
+        vector<vector<int>> dp(prices.size()+1,vector<int>(5,0));
+        for(int i=prices.size()-1;i>=0;i--){
+            for(int j=actions.size()-1;j>=0;j--){
+                        int first = prices[i]*actions[j] + dp[i+1][j+1];
+                        int second =  dp[i+1][j];
+                        dp[i][j]=max(first,second);
+            }
+        }
+        return dp[0][0];
     }
 };
