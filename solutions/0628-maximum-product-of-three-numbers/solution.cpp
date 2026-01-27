11d7c7a09e9335047f9cdeda044bda240e8f781d
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2026-01-27 09:37:50 +0000

    [LeetCode Sync] Runtime - 14 ms (26.98%), Memory - 31.5 MB (41.68%)

--- solutions/0628-maximum-product-of-three-numbers/solution.cpp
+++ solutions/0628-maximum-product-of-three-numbers/solution.cpp
@@ -2,13 +2,12 @@ class Solution {
 public:
     int maximumProduct(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        int maxP = INT_MIN;
-        for(int i=1;i<nums.size()-1;i++){
-            int a = nums[i];
-            int b = nums[i-1];
-            int c = nums[nums.size()-1];
-            maxP = max(maxP,a*b*c);
-        }
-        return maxP;
+        // one possiblity - max three numbers 
+        int a = nums[nums.size()-3]*nums[nums.size()-2]*nums[nums.size()-1];
+        // one possiblity - maximum negative two numbers and one maximum positive number
+        int b = nums[0]*nums[1]*nums[nums.size()-1];
+        // when all are negative in that case -
+        int c = nums[0]*nums[1]*nums[2];
+        return max({a,b,c});
     }
 };
