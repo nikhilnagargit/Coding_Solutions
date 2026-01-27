308791b5d4c5b69cfd473f209d576e8f8ee67ffb
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2026-01-27 09:29:10 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.1 MB (49.27%)

--- solutions/0678-valid-parenthesis-string/solution.cpp
+++ solutions/0678-valid-parenthesis-string/solution.cpp
@@ -1,34 +1,18 @@
 class Solution {
 public:
-
     bool checkValidString(string s) {
         int openCount = 0 ;
-        // left to right pass
-        for (auto c: s){
-            if(c==')'){
-                openCount--;
-            }
-            else if(c=='(' or c=='*'){
-                openCount++;
-            }
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='(' or s[i]=='*') openCount++;
+            else openCount--;
             if(openCount<0) return false;
         }
-
-        int closeCount =0 ;
-        // right to left pass
+        int closeCount =0;
         for(int i=s.size()-1;i>=0;i--){
-            char c= s[i];
-             if(c==')' or c=='*'){
-                closeCount++;
-            }
-            else if(c=='('){
-                closeCount--;
-            }
-            if(closeCount<0){
-                return false;
-            }
+            if(s[i]==')' or s[i]=='*') closeCount++;
+            else closeCount--;
+            if(closeCount<0) return false;
         }
-
         return true;
     }
 };
