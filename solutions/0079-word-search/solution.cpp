ba44aab3804ef954a5f5019038dc946a0b004feb
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2026-01-29 07:44:19 +0000

    [LeetCode Sync] Runtime - 296 ms (49.73%), Memory - 10.8 MB (53.48%)

--- solutions/0079-word-search/solution.cpp
+++ solutions/0079-word-search/solution.cpp
@@ -1,31 +1,37 @@
 class Solution {
 public:
-    bool dfs(vector<vector<char>>& board, string& word,int idx,int i,int j,vector<vector<int>>& visited){
-                if(idx==word.size()){
+ vector<pair<int,int>> dir;
+    bool solve(vector<vector<char>>& board, int i,int j, string& word, int word_idx, vector<vector<int>>& visited){
+        if(word_idx==word.size()){
             return true;
         }
-        if(i<0 or j<0 or i>=board.size() or j>=board[0].size()){
+        if(i<0 or i>=board.size() or j<0 or j>=board[0].size()){
             return false;
         }
-
         if(visited[i][j]){
             return false;
         }
-        if(word[idx]!=board[i][j])return false;
+       
+        if(board[i][j]!=word[word_idx]){
+            return false;
+        }
 
-        visited[i][j]=1;
-        if(dfs(board,word,idx+1,i+1,j,visited)) return true;
-        if(dfs(board,word,idx+1,i,j+1,visited)) return true;
-        if(dfs(board,word,idx+1,i-1,j,visited))return true;
-        if(dfs(board,word,idx+1,i,j-1,visited))return true;
-        visited[i][j]=0;
+        visited[i][j] = 1;
+        for(auto d : dir){
+            if(solve(board,i+d.first,j+d.second,word,word_idx+1,visited)){
+                return true;
+            }
+        }
+        visited[i][j] = 0;
         return false;
     }
     bool exist(vector<vector<char>>& board, string word) {
+        // if we find not matching char, stop traversing. do this for all the cells
+        dir = {{0,1},{1,0},{0,-1},{-1,0}};
         vector<vector<int>> visited(board.size(),vector<int>(board[0].size(),0));
         for(int i=0;i<board.size();i++){
             for(int j=0;j<board[0].size();j++){
-                if(dfs(board,word,0,i,j,visited)) return true;
+                if(solve(board,i,j,word,0,visited)) return true;
             }
         }
         return false;
