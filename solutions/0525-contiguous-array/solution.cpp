db3bca422097d4608837e9a69f0045d365081457
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2026-01-30 06:15:01 +0000

    [LeetCode Sync] Runtime - 22 ms (70.04%), Memory - 88.4 MB (44.77%)

--- solutions/0525-contiguous-array/solution.cpp
+++ solutions/0525-contiguous-array/solution.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
     int findMaxLength(vector<int>& nums) {
-        // diff -> index map
-        unordered_map<int,int> m;
-        int zeros = 0;
         int ones = 0;
-        int maxL = 0;
+        int zeros = 0;
+        int maxLen = 0;
+        unordered_map<int,int> m;
         m[0]=-1;
         for(int i=0;i<nums.size();i++){
             if(nums[i]==0) zeros++;
             else ones++;
-
-            int diff = zeros - ones;
-
-            if(m.count(diff)){
-                maxL = max(maxL, i-m[diff]);
+            int key = ones-zeros;
+            if(m.find(key)!=m.end()){
+                maxLen = max(maxLen,i-m[key]);
             }
             else{
-                m[diff]=i;
+                m[key]=i;
             }
         }
-        return maxL;
+        return maxLen;
     }
 };
