5df84994b4d652f104832c8ca3fa760056ced979
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2026-01-30 08:04:08 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 15.3 MB (34.57%)

--- solutions/0033-search-in-rotated-sorted-array/solution.cpp
+++ solutions/0033-search-in-rotated-sorted-array/solution.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int left = 0;
-        int right= nums.size()-1;
-        int mid = 0;
+        int left =0 ;
+        int right =nums.size()-1;
         while(left<=right){
-            mid = left + (right-left)/2;
-            if(nums[mid]==target){
-                return mid;
-            }
-            else if(nums[left]>nums[mid]){
-                if(target>=nums[mid] and target<=nums[right]){
-                left =  mid+1;
-                }
-                else{
-                 
-                           right = mid-1;
-                }
-            }
-            else{
-                if(nums[left]<=target and target<=nums[mid]){
+            int mid = left + (right-left)/2;
+            if(nums[mid]==target) return mid;
+            else if(nums[left]<=nums[mid]){
+                // breakpoint is in right section
+                if(target>=nums[left] and target<nums[mid])
+                {
+                    // move left
                     right = mid-1;
                 }
                 else{
                     left = mid+1;
                 }
             }
-  
+            else{
+                // breakpoitn in in left section
+                if(target>nums[mid] and target<=nums[right]){
+                    // move right section
+                    left = mid+1;
+                }
+                else{
+                    right = mid-1;
+                }
+            }
         }
-
         return -1;
     }
 };
