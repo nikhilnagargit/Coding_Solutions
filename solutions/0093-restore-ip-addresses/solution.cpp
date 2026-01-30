5ae847b34ac37c7f94c81b7be79881bb3b77fc9b
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2026-01-30 05:56:22 +0000

    [LeetCode Sync] Runtime - 3 ms (31.98%), Memory - 10.9 MB (36.25%)

--- solutions/0093-restore-ip-addresses/solution.cpp
+++ solutions/0093-restore-ip-addresses/solution.cpp
@@ -1,37 +1,57 @@
 class Solution {
 public:
-    bool isValid(string part){
-        if(part.size()>1 and part[0]=='0') return false;
-        if(stoi(part)<256)return true;
-        return false;
+    vector<string> ans;
+    // check if a formed item is valid
+    bool isValidItem(string item){
+        // no prefix zero
+        if(item=="")return false;
+        if(item.size()>1 and item[0]=='0')return false;
+        if(stoi(item)>255)return false;
+        return true;
     }
-    void solve(string& s, int i, int dots, string prefix,vector<string>& ans){
-        // base case
-        if(dots==4 and i==s.size()){
-            // success
-            ans.push_back(prefix.substr(0,prefix.size()-1));
+    void solve(string& s, int i, int remainingDots ,vector<string>& v){
+        // base cases
+        if(i==s.size()){
             return;
         }
-        if(dots>4){
+        if(remainingDots<0){
             return;
         }
-        for(int j=0;j<3;j++){
-            // three digits we are gonna take and check if they are valid
-            if(i+j>=s.size()){
-                break;
+        if(remainingDots==0 and i<s.size()){
+            string item = s.substr(i);
+            if(isValidItem(item)){
+                v.push_back(item);
+                // form the ip address and push in answer
+                string temp = "";
+                for(int j=0;j<v.size();j++){
+                    if(j!=v.size()-1)
+                    temp+= v[j] + ".";
+                    else
+                    temp+= v[j];
+                }
+                ans.push_back(temp);
+                v.pop_back();
+                return;
             }
-            string part = s.substr(i,j+1);
-            if(isValid(part)){
-                solve(s,i+j+1,dots+1,prefix+part+".",ans);
+        }
+        for(int l=1;l<=3;l++){
+            if(i+l>=s.size()){
+                continue;
+            }
+            string item = s.substr(i,l);
+            if(isValidItem(item)){
+                v.push_back(item);
+                solve(s,i+l,remainingDots-1,v);
+                v.pop_back();
             }
         }
     }
+
     vector<string> restoreIpAddresses(string s) {
-        vector<string> ans;
-        if(s.size()>12 or s.size()<4) return ans;
-        int dots = 0;
-        string prefix = "";
-        solve(s,0,dots,prefix,ans);
+        if(s.size()<4 or s.size()>12)return ans;
+        int remainingDots = 3;
+        vector<string> v;
+        solve(s,0,remainingDots,v);
         return ans;
     }
 };
