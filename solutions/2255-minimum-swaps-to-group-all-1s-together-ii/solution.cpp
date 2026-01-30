87cd31286d51aedc54fa8aab62acea499ba65179
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2026-01-30 07:03:27 +0000

    [LeetCode Sync] Runtime - 7 ms (62.89%), Memory - 95.1 MB (8.23%)

--- solutions/2255-minimum-swaps-to-group-all-1s-together-ii/solution.cpp
+++ solutions/2255-minimum-swaps-to-group-all-1s-together-ii/solution.cpp
@@ -1,46 +1,35 @@
 class Solution {
 public:
     int minSwaps(vector<int>& nums) {
-        // find the min zero in sliding window of circular suffix added array
-        int x = 0;
-    
-        for(auto& item: nums){
-            if(item==1) x++;
+        // append the n-1 items again back to the nums and use sliding window
+        vector<int> v (nums.begin(),nums.end());
+
+        for(int i=0;i<nums.size()-1;i++){
+            v.push_back(nums[i]);
         }
-        int zeros= 0;
-        for(int i=0;i<x;i++){
-            if(nums[i]==0)zeros++;
-        }
-        int minZeros = zeros;
-        for(int i=x;i<nums.size();i++){
-            if(nums[i]==0){
-                zeros++;
+        int K = 0 ;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==1) K++;
+        }  
+
+        int minZeros = INT_MAX;
+        int left = 0 ;
+        int zeros =0 ;
+        int ones = 0 ;
+        for(int right=0;right<v.size();right++){
+            if(v[right]==1) ones++;
+            else zeros++;
+            if(right-left+1<K){
+                continue;
             }
-            if(nums[i-x]==0){
-                zeros--;
+            while(right-left+1>K){
+                if(v[left]==0) zeros--;
+                else ones--;
+                left++;
             }
             minZeros = min(minZeros,zeros);
         }
-        int n = nums.size();
-
-
-        // try with other window
-        zeros = 0;
-        
-        for(int i=0;i<n-x;i++){
-            if(nums[i]==0)zeros++;
-        }
-        int maxZeros = zeros;
-
-        for(int i=n-x;i<nums.size();i++){
-            if(nums[i]==0){
-                zeros++;
-            }
-            if(nums[i-(n-x)]==0){
-                zeros--;
-            }
-            maxZeros = max(maxZeros,zeros);
-        }
-        return min(minZeros, (n-x)-maxZeros);
+        return minZeros;
     }
+
 };
