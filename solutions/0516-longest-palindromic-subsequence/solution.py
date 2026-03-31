999c0ae515c340d090d434ac8cb31799ff0c7453
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2026-03-31 08:27:01 +0000

    [LeetCode Sync] Runtime - 628 ms (73.64%), Memory - 258.7 MB (6.34%)

---
+++ solutions/0516-longest-palindromic-subsequence/solution.py
@@ -0,0 +1,19 @@
+from functools import lru_cache
+
+class Solution:
+    def longestPalindromeSubseq(self, s: str) -> int:
+        n = len(s)
+
+        @lru_cache(maxsize=None)
+        def solve(i: int, j: int) -> int:
+            if i > j:
+                return 0
+            if i == j:
+                return 1
+
+            if s[i] == s[j]:
+                return 2 + solve(i + 1, j - 1)
+
+            return max(solve(i + 1, j), solve(i, j - 1))
+
+        return solve(0, n - 1)
