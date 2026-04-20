0af24f91f92edaeb22198fe695ae85d853d52fbb
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2026-04-20 07:11:32 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.3 MB (72.57%)

--- solutions/0496-next-greater-element-i/solution.py
+++ solutions/0496-next-greater-element-i/solution.py
@@ -1,13 +1,28 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        # accumulate numbers in stack , and pop if next greater is found
         stk = []
-        next_greater = {}
-        for n in reversed(nums2):
-            while stk and stk[-1]<n:
+        nextgreater = {}
+        for val in (nums2):
+            while len(stk)!=0 and stk[-1]<val:
+                item = stk[-1]
                 stk.pop()
-            next_greater[n] = stk[-1] if stk else -1
-            stk.append(n)
-        return [next_greater[i] for i in nums1]
+                nextgreater[item] = val
+            stk.append(val)
+
+        ans = []
+        for i in nums1:
+            if i in nextgreater:
+                ans.append(nextgreater[i])
+            else:
+                ans.append(-1)
+        return ans
+
+# nums2 = 1,3,4,2
+#ng = {}
+
+        
+
 
 
             
