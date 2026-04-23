3f27e6761ae18086404f21aef2844c8c2ef04f17
Nikhil Nagar <44896376+nikhilnagargit@users.noreply.github.com>
2026-04-23 05:26:35 +0000

    [LeetCode Sync] Runtime - 123 ms (81.10%), Memory - 54 MB (74.02%)

---
+++ solutions/2721-sum-of-distances/solution.py
@@ -0,0 +1,29 @@
+class Solution:
+    def distance(self, nums: List[int]) -> List[int]:
+        m = {}
+        for i,value in enumerate(nums):
+            if value in m:
+                m[value].append(i)
+            else:
+                m[value] = [i]
+
+        ans = [0 for _ in range(len(nums))]
+        # loop on each key value pair of that dict.
+        for k,l in m.items():
+            if len(l)>1:
+                # process l and get each value and put in ans
+                arr = l
+                leftsum = 0
+                totalsum = sum(l)
+                for i in range(len(l)):
+                    leftsum = leftsum +( l[i-1] if i>0 else 0)
+                    rightsum = totalsum - leftsum - l[i]
+                    leftvalue = abs(leftsum - (i)*l[i])
+                    rightvalue = abs(rightsum - (len(l)-i-1)*l[i] )
+                    ans[l[i]]=rightvalue+leftvalue
+        return ans
+        
+
+
+
+        
